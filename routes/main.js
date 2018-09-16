ac2db51a612235447de60a03d26329bfb4328d5c
Tien Linda Vu <lindavu@Tiens-MacBook-Pro.local>
2018-09-16 16:18:43 -0400

    server pagination file set up

---
+++ routes/main.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const faker = require('faker')
+const Product = require('../models/product')
+
+router.get('/generate-fake-data', (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product()
+
+    product.category = faker.commerce.department()
+    product.name = faker.commerce.productName()
+    product.price = faker.commerce.price()
+    product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+
+    product.save((err) => {
+      if (err) throw err
+    })
+  }
+  res.end()
+})
+
+router.get('/products', (req, res, next) => {
+    const perPage = 9
+  
+    // return the first page by default
+    const page = req.query.page || 1
+  
+    Product
+      .find({})
+      .skip((perPage * page) - perPage)
+      .limit(perPage)
+      .exec((err, products) => {
+        // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
+        Product.count().exec((err, count) => {
+          if (err) return next(err)
+  
+          res.send(products)
+        })
+      })
+  })
+
+module.exports = router

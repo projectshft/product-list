84eeecdb4bb05388b158d3d8fbd2c6bd9d8d7ef5
AaronStearns <stearnsconcepts@gmail.com>
2018-09-23 19:52:57 -0400

    modifying routes in main.js

---
+++ product-list-app/src/reducers/product-reducer.js
@@ -0,0 +1,11 @@
+import {FETCH_PRODUCTS} from '../actions/index'
+
+
+export default function (state=[], action) { 
+    switch(action.type){
+        case FETCH_PRODUCTS:
+            return action.payload.data
+        default:
+            return state
+    }
+}

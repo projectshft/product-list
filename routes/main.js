4f599b522861e2ced0d4e732611949d8a69378e3
Charles Ferringer <charlesferringer@yahoo.com>
2018-09-25 21:58:06 -0400

    Adding react-app

--- routes/main.js
+++ routes/main.js
@@ -19,27 +19,44 @@ router.get('/generate-fake-data', (req, res, next) => {
 })
 
 router.get('/products', (req, res, next) => {
-    let pageLimit = 9
+    const pageLimit = 9
+    const page = req.query.page || 1;
+
+    Product
+      .find({})
+      .skip((pageLimit * page) - perPage)
+      .limit(pageLimit)
+      .exec((err, products) => {
+        Product.count().exec((err, count) => {
+          if (err) return next(err)
+          res.send(products)
+        })
+      })
 })
 
 router.get('/products/:productId', (req, res, next) => {
-
+  res.send()
+  res.end()
 })
 
 router.post('/products', (req, res, next) => {
-
+  res.send()
+  res.end()
 })
 
 router.post('/:productId/reviews', (req, res, next) => {
-
+  res.send()
+  res.end()
 })
 
 router.delete('/products/:productId', (req, res, next) => {
-
+  res.send()
+  res.end()
 })
 
 router.delete('/reviews/:reviewId', (req, res, next) => {
-
+  res.send()
+  res.end()
 })
 
 module.exports = router

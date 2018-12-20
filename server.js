93c8821452f7b935f4bdfb39a9aec22af221f0e9
James DeVore <jdevore4592@gmail.com>
2018-12-20 13:46:11 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost/scamazon',{useNewUrlParser:true})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const mainRoutes = require('./routes/main')
+
+app.use(mainRoutes)
+
+app.listen(8000, () => {
+  console.log('listening on port' + 8000)
+})
+

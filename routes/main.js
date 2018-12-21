e5c51510b80dc713c898aa4c496d8445e7127798
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-12-21 11:53:07 -0500

    Add routes: get product by _id, get all reviews (paginated), and post a product

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,13 @@ const faker = require('faker')
 const Product = require('../models/product')
 const Review = require('../models/review')
 
+//Generate random products, with random reviews (between 1-3) on each product
 router.get('/generate-fake-data', (req,res,next) => {
+
    for (let i = 0; i < 90 ; i++) {
       let product = new Product()
       let randomNumberOfReviews = Math.floor(Math.random() * 3);
 
-      product.id = (i+1);
       product.reviews = [];
       product.category = faker.commerce.department();
       product.name = faker.commerce.productName();
@@ -35,9 +36,10 @@ router.get('/generate-fake-data', (req,res,next) => {
    res.end()
 })
 
+// GET /products Returns products based on page and limit query parameters.
 router.get('/products', (req,res,next) => {
    let page = parseInt(req.query.page) || 1;
-   let limit = parseInt(req.query.limit) || 10;
+   let limit = parseInt(req.query.limit) || 9;
    let itemsToSkip = (page - 1) * limit;
 
    Product
@@ -47,10 +49,77 @@ router.get('/products', (req,res,next) => {
       .populate('reviews')
       .exec((err, products) => {
          Product.count().exec((err, count) => {
-            if (err) return next(err)
-            res.send(products)
+            if (err) throw err
+            res.send({number_items: count, number_pages: Math.ceil(count/limit), current_page: page, products})
+            res.end()
          })
       })
 })
+
+// GET /products/:productId Returns a specific product by it's id
+router.get('/products/:productId', (req,res,next) => {
+
+   Product
+      .find({_id: req.params.productId })
+      .populate('reviews')
+      .exec((err, products) => {
+         if (err) throw err
+         res.send(products[0])
+         res.end()
+      })
+})
+
+// GET /reviews Returns ALL the reviews, but limited to 40 at a time.
+router.get('/reviews', (req,res,next) => {
+   let page = parseInt(req.query.page) || 1;
+   let limit = parseInt(req.query.limit) || 40;
+   let itemsToSkip = (page - 1) * limit;
+
+   Review
+      .find()
+      .skip(itemsToSkip)
+      .limit(limit)
+      .populate('product')
+      .exec((err, reviews) => {
+         Review.count().exec((err, count) => {
+            if (err) throw err
+
+            res.send({number_items: count, number_pages: Math.ceil(count/limit), current_page: page, reviews})
+            res.end()
+         })
+      })
+})
+
+// POST /products Creates a new product in the database
+router.post('/products', (req, res, next) => {
+   let productToAdd = new Product ();
+
+   productToAdd.reviews = [];
+   productToAdd.category = req.body.category;
+   productToAdd.name = req.body.name;
+   productToAdd.price = req.body.price;
+   productToAdd.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png';
+
+   productToAdd.save((err, product) => {
+      if (err) throw err
+      res.send(product)
+      res.end()
+   })
+})
+
+// POST /:productId/reviews Creates a new review in the database by adding it to the correct product's reviews array.
+// router.post('/:productId/reviews', (req, res, next) => {
+//    let reviewToAdd = new Review ();
+
+
+//    reviewToAdd.save((err, review) => {
+//       if (err) throw err
+//       res.send(review)
+//       res.end()
+//    })
+// })
+
+// DELETE /products/:product Deletes a product by id
+// DELETE /reviews/:review Deletes a review by id
 
 module.exports = router

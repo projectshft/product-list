59b8824a7b79b7c87bc121d13d8754dc9959f9fb
James DeVore <jdevore4592@gmail.com>
2018-12-22 18:56:06 -0500

    refactored get request

--- routes/main.js
+++ routes/main.js
@@ -10,10 +10,10 @@ Generate and populate database with data
 NO LONGER NEEDED DATA AS OF 12/20/18 3:11 PM
 =====================================================*/
 // router.get("/generate-fake-data", async (req, res, next) => {
-  
 
-    
-    
+
+
+
 //   for (let i = 0; i < 500; i++) {
 //     let productImg = await fetch("https://picsum.photos/200/300/?random").then(r => r.url);
 //     let numberOfReviews = Math.floor(Math.random()*20)
@@ -48,7 +48,7 @@ NO LONGER NEEDED DATA AS OF 12/20/18 3:11 PM
 // });
 
 router.param("product", async function (req, res, next, id) {
-//this middleware grabs the matching product and adds it to the request
+  //this middleware grabs the matching product and adds it to the request
   req.product = await Product.findById(id).populate('reviews').exec();
 
   next();
@@ -60,64 +60,51 @@ router.param("review", async function (req, res, next, id) {
   next();
 });
 
-router.get('/products', async (req,res, next) => {
-//Setting up all the variables and search terms
+router.get('/products', async (req, res, next) => {
+  //Setting up all the variables and search terms
 
   let search = req.query.search
   let page = req.query.page || 1;
   page = parseInt(page)
   let mongoQueryObj = {};
   //To ensure a category was selected and isn't somehow undefined
-  if(req.query.category && req.query.category !== "All"){
+  if (req.query.category && req.query.category !== "All") {
     mongoQueryObj.category = req.query.category
   }
   //default to ascending
   let price = req.query.price
-  if(!price){
+  if (!price) {
     price = 1
   } else {
     price = parseInt(price)
   }
-  
-  const perPage = 12;
-  //this works but badly needs refactoring 12/22/18
-  if(!search){
-    Product.find(mongoQueryObj)
-      .sort({ price: price }).exec((err,result) => {    
-        res.send({
-          total_products: result.length,
-          url:req.originalUrl,
-          page_count: (Math.floor(result.length / perPage)),
-          page_number: page,
-          products: result.slice(page-1, page+11)
-        })
-      })
-  }
-  else {
-    Object.assign(mongoQueryObj, {$text: { $search: search, $caseSensitive: false } })
-    Product.find(mongoQueryObj)
-      .sort({ price: price })
-      .skip((page * perPage) - perPage).exec( (err, result) => {
-        res.send({
-          total_products: result.length,
-          url:req.originalUrl,
-          page_count: (Math.floor(result.length / perPage)),
-          page_number: page,
-          products: result.slice(page - 1, page + 11)
-        })
-      })
-   }
-})
 
+  const perPage = 12;
+
+  if (search) {
+    Object.assign(mongoQueryObj, { $text: { $search: search, $caseSensitive: false } })
+  }
+  Product.find(mongoQueryObj)
+    .sort({ price: price }).exec((err, result) => {
+      res.send({
+        total_products: result.length,
+        url: req.originalUrl,
+        page_count: (Math.floor(result.length / perPage)),
+        page_number: page,
+        products: result.slice(page - 1, page + 11)
+      })
+    })
+})
 
 router.get('/products/:product', (req, res) => {
 
-//uses the param middleware to find the specific product by ID
+  //uses the param middleware to find the specific product by ID
   res.send(req.product)
+
 })
 
 
-router.get('/reviews', async (req,res) => {
+router.get('/reviews', async (req, res) => {
 
   let { page } = req.query || 1;
   const perPage = 40;
@@ -130,44 +117,43 @@ router.post('/products', async (req, res) => {
   let product = req.body
   //try and add the item to the database
 
-    let newProduct = new Product ({
-      category: product.category,
-      name: product.name,
-      price: product.price,
-      image: product.image,
-      description: product.description,
-      reviews:[]
-    })
-    newProduct.save()
+  let newProduct = new Product({
+    category: product.category,
+    name: product.name,
+    price: product.price,
+    image: product.image,
+    description: product.description,
+    reviews: []
+  })
+  newProduct.save()
     .then(result => res.send("Product Saved"))
     .catch(err => res.status(400).send(err))
 })
 
-router.post("/products/:product/reviews", (req,res) => {
+router.post("/products/:product/reviews", (req, res) => {
   let review = new Review({
-    username:req.body.username,
-    text:req.body.text,
-    rating:req.body.rating,
-    product:req.product._id
+    username: req.body.username,
+    text: req.body.text,
+    rating: req.body.rating,
+    product: req.product._id
   })
   req.product.reviews.push(review)
   req.product.save()
-  .then(() => review.save())
-  .then(result => res.send("review saved to database"))
-  .catch(err => res.send(err));
+    .then(() => review.save())
+    .then(result => res.send("review saved to database"))
+    .catch(err => res.send(err));
 });
 
-router.delete('/products/:product/reviews/:review' , async (req,res) => {
-  
-  await Product.updateOne({'_id':req.product._id},
-  {$pull:{'reviews':req.review._id}}).exec()
+router.delete('/products/:product/reviews/:review', async (req, res) => {
+
+  await Product.updateOne({ '_id': req.product._id },
+    { $pull: { 'reviews': req.review._id } }).exec()
 
   await req.product.save()
   await Review.findByIdAndDelete(req.review._id).exec()
   await req.review.save()
   res.send()
 
-  
 })
 
 router.delete('/products/:product', async (req, res) => {

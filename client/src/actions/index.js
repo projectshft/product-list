e3fd3451f4c0472aecbf30f6c4f9dbc620444b6f
Edward Barton <edward.barton@icloud.com>
2018-12-26 11:58:56 -0500

    Implemented product pagination

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 const url = `http://localhost:8000/products`;
 
 export const fetchProducts = async query => {
-  const request = await fetch(url);
+  const request = await fetch(url + (query ? query : ''));
   const { docs: products, total, pages, page, limit } = await request.json();
   return {
     type: FETCH_PRODUCTS,
--- client/src/containers/product-list.js
+++ client/src/containers/product-list.js
@@ -2,27 +2,69 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchProducts } from '../actions';
+import ReactPaginate from 'react-paginate';
 
 class ProductList extends Component {
   componentDidMount() {
     this.props.fetchProducts();
   }
 
+  handlePageClick = data => {
+    let pageNum = data.selected + 1;
+    this.props.fetchProducts(`?page=${pageNum}`);
+  };
+
   render() {
-    const { products } = this.props;
+    const { products, pages } = this.props;
+
     return (
-      <ul className="list-group col-sm-4">
-        {products &&
-          products.map(d => (
-            <li
-              key={d._id}
-              onClick={() => console.log('poop')}
-              className="list-group-item"
-            >
-              {d.name}
-            </li>
-          ))}
-      </ul>
+      <div className="container">
+        <div className="row filters">{/* CATEGORY / PRICE SORT */}</div>
+        <div className="row products">
+          {products &&
+            products.map(p => (
+              <div key={p._id} className="col-md-4">
+                <div className="card my-3">
+                  <img
+                    src={p.image}
+                    alt="Kitten"
+                    style={{ width: '100%', height: '50%' }}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">
+                      <strong>Price: </strong>${p.price}
+                    </p>
+                    <p className="card-text">
+                      <strong>Category: </strong>
+                      {p.category}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        <div className="row pages">
+          <ReactPaginate
+            previousLabel={'Prev'}
+            nextLabel={'Next'}
+            breakLabel={'...'}
+            breakClassName={'page-link'}
+            pageCount={pages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination justify-content-center'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            activeClassName={'active'}
+            previousClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+          />
+        </div>
+      </div>
     );
   }
 }

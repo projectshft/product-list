8a682057243a8f6de03be4b577bf7dda3ae8d1b8
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-12-29 21:32:01 -0500

    Update search_information component to display "All Products" and "All Categories" when appropriate

--- front-end/src/components/search_information.js
+++ front-end/src/components/search_information.js
@@ -2,14 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 
 const SearchInformation = (props) => {
-   
-   return (
-      <div className="container">
-         <p>Searching for: <strong>{ props.params.search }</strong> in <strong>{ props.params.category }</strong></p>
-         <p>Total number of items: <strong>{ props.products.number_items }</strong></p>
-         <p>Viewing page: <strong>{ props.products.current_page }</strong> of <strong>{ props.products.number_pages }</strong></p>
-      </div>
-   );
+   const searchExist = props.params.search;
+   const categorySelected = props.params.category;
+   let searchingForLine;
+
+   if (searchExist && categorySelected ) {
+      searchingForLine = <p>Searching for: <strong>{ props.params.search }</strong> in <strong>{ props.params.category }</strong></p>;
+   } else if ( searchExist && !categorySelected ) {
+      searchingForLine = <p>Searching for: <strong>{ props.params.search }</strong> in <strong>All Categories</strong></p>;
+   } else if ( !searchExist && categorySelected ) {
+      searchingForLine = <p>Searching for: <strong>All Products</strong> in <strong>{ props.params.category }</strong></p>;
+   } else if ( !searchExist && !categorySelected ) {
+      searchingForLine = <p>Searching for: <strong>All Products</strong> in <strong>All Categories</strong></p>;
+   }
+
+      return (
+         <div className="container">
+            {searchingForLine}
+            <p>Total number of items: <strong>{ props.products.number_items }</strong></p>
+            <p>Viewing page: <strong>{ props.products.current_page }</strong> of <strong>{ props.products.number_pages }</strong></p>
+         </div>
+      );
 }
 
 function mapStateToProps(state) {

13ec9dcf35fc8a5532c8417b21f58b28f8e10313
Rhys Lutsky <rhyslutsky@gmail.com>
2019-02-04 11:31:45 -0500

    update index and app for react only; add hardcoded data to client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 //Provider binds Redux to React & makes store available to all containers
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+// import {Provider} from 'react-redux';
+// import {createStore} from 'redux';
 
 import App from "./components/app";
 // import rootReducer from "./reducers";
 // import Products from "./components/product";
 
 import './index.css';
-import reducerProducts from './reducers/reducer-products';
+// import reducerProducts from './reducers/reducer-products';
 
-const store = createStore(reducerProducts);
+// const store = createStore(reducerProducts);
 
 ReactDOM.render(
-    <Provider store={store}> 
-        <App />
-    </Provider>, document.getElementById('root'));
+    <App />
+    , document.getElementById('root')
+);
 
 

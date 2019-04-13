6d43eef479db7c7d2de6c303a0d195009adfc494
Jason Doss <doss2k@gmail.com>
2019-04-13 12:49:51 -0400

    Change default image

--- server/routes/main.js
+++ server/routes/main.js
@@ -15,14 +15,11 @@ router.get('/generate-fake-data', (request, response, next) => {
     product.category = faker.commerce.department();
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
-    product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png';
+    product.image = faker.image.nature();
     product.reviews = [];
 
-  // Get random number of reviews for each product
-  const randomReviewAmount = Math.floor(Math.random() * 5);
-
-  // Create random number of reviews, save them, and push them into product reviews
-  for (let j = 0; j < randomReviewAmount; j++) {
+  // Creates 3 random reviews, saves them, and pushes them into product reviews
+  for (let j = 0; j < 3; j++) {
     let review = new Review ({
       userName: faker.internet.userName(),
       text: faker.lorem.sentences(2),

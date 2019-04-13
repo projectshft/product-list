ac073dcc7e9ed43911d527a9992262d6b4284668
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-04-13 09:59:00 -0400

    setting up router, Provider and middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)

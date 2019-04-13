222173451536c498b4c0f203edba60de6de57888
Knox <knox.brand@gmail.com>
2019-04-13 11:22:31 -0400

    server side routes all setup with some level of input validation and error checking.

--- routes/main.js
+++ routes/main.js
@@ -3,29 +3,76 @@ const faker = require("faker");
 const Product = require("../models/product");
 const Review = require("../models/review");
 
+//Initial db populating with fake data
+// router.get('/generate-fake-data', (req, res, next) => {
+//   for (let i = 0; i < 90; i++) {
+//     let product = new Product()
+
+//     product.category = faker.commerce.department()
+//     product.name = faker.commerce.productName()
+//     product.price = faker.commerce.price()
+//     product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+//     product.reviews = []
+
+//     let review = new Review()
+//     review.userName = faker.name.firstName();
+//     review.text = faker.lorem.sentence();
+//     review.product = product._id
+
+//     review.save();
+//     product.reviews.push(review);
+
+//     product.save((err) => {
+//       if (err) throw err
+//     })
+//   }
+//   res.end()
+// })
+
+//To uppercase the first letter of query strings so they match our db categories.
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 router.get("/products", (req, res, next) => {
+  const { page, category, price } = req.query;
   const perPage = 9;
   // return the first page by default
-  const page = req.query.page || 1;
-  Product.find({})
-    .skip(perPage * page - perPage)
+  const pageNumber = page || 1;
+  // Empty object by default, query category if one exists. 
+  // Capitalize is to cap first letter of cat name since query comes in as all lowercase. Categories are in db with first letter capitalized.
+  const findQuery = category ? { category: category.capitalize() } : {};
+  // Sort query string value to pass to .sort(). If string exists and is not highest or lowest, returns "Invalid" so we can identify the bad query and return 400.
+  const sortQuery = price === "highest" ? { price: "desc"} : price === "lowest" ? { price: "asc"} : price.length > 0 ? "Invalid" : null
+  if (sortQuery === "Invalid") {
+     return res.status(400).send("Invalid sort query.");
+    }
+   Product.find(findQuery)
+    .skip(perPage * pageNumber - perPage)
     .limit(perPage)
+    .sort(sortQuery)
     .exec((err, products) => {
       // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
       Product.estimatedDocumentCount().exec((err, count) => {
         if (err) return next(err);
-
         res.send(products);
       });
     });
 });
 
 router.get("/products/:product", (req, res) => {
-  requestedId = req.params.product;
-  Product.findById(requestedId).exec((err, product) => {
-    // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
-    res.send(product);
-  });
+  productId = req.params.product;
+  if (productId.length === 24) {
+    Product.findById(productId, (err, product) => {
+      if (product) {
+        res.status(200).send(product);
+      } else {
+        return res.status(404).send("Product ID not found.");
+      }
+    });
+  } else {
+    return res.status(400).send("Invalid product ID.");
+  }
 });
 
 router.get("/reviews", (req, res, next) => {
@@ -46,44 +93,83 @@ router.get("/reviews", (req, res, next) => {
 });
 
 router.post("/products", (req, res) => {
-  product = new Product();
-  product.category = req.body.category;
-  product.name = req.body.name;
-  product.price = req.body.price;
-  product.image = req.body.imageUrl;
-  product.reviews = [];
-  product.save();
-  res.send(product);
+  const { category, name, price, imageUrl } = req.body;
+  if (
+    category.length > 0 &&
+    name.length > 0 &&
+    price.length > 0 &&
+    imageUrl.length > 0
+  ) {
+    product = new Product();
+    product.category = category;
+    product.name = name;
+    product.price = price;
+    product.image = imageUrl;
+    product.reviews = [];
+    product.save();
+    res.status(200).send(product);
+  } else {
+    return res.status(400).send("Invalid product data.");
+  }
 });
 
 router.post("/:product/reviews", (req, res) => {
+  const { text, userName } = req.body;
   const productId = req.params.product;
-  review = new Review();
-  review.userName = req.body.userName;
-  review.text = req.body.text;
-  review.product = productId;
-  review.save();
-  Product.findByIdAndUpdate(productId, {$push: {reviews: review}})
-  .exec()
-  res.send(review);
+  if (productId.length === 24) {
+    if (text.length > 0 && userName.length > 0) {
+      review = new Review();
+      review.userName = userName;
+      review.text = text;
+      review.product = productId;
+      Product.findByIdAndUpdate(
+        productId,
+        { $push: { reviews: review } },
+        (err, product) => {
+          if (product) {
+            review.save();
+            res.status(200).send(review);
+          } else {
+            return res.status(404).send("Product ID not found.");
+          }
+        }
+      );
+    } else {
+      return res.status(400).send("Invalid userName or text.");
+    }
+  } else {
+    return res.status(400).send("Invalid product ID.");
+  }
 });
 
 router.delete("/products/:product", (req, res) => {
   const productId = req.params.product;
-  Product.findByIdAndDelete(productId, (err, product) => {
-    if (product) {
-      res.status(200).send(productId + " was deleted.")
-    } else {
-      return res.status(404).send("Id not found error.")
-    }
-  })
+  if (productId.length === 24) {
+    Product.findByIdAndDelete(productId, (err, product) => {
+      if (product) {
+        res.status(200).send(productId + " successfully deleted.");
+      } else {
+        return res.status(404).send("Product ID not found.");
+      }
+    });
+  } else {
+    return res.status(400).send("Invalid product ID.");
+  }
 });
 
 router.delete("/reviews/:review", (req, res) => {
   const reviewId = req.params.review;
-  Review.findByIdAndDelete(reviewId)
-  .exec()
-  res.send(reviewId + " was deleted.")
+  if (reviewId.length === 24) {
+    Review.findByIdAndDelete(reviewId, (err, review) => {
+      if (review) {
+        res.status(200).send(reviewId + " successfully deleted.");
+      } else {
+        return res.status(404).send("Review ID not found.");
+      }
+    });
+  } else {
+    return res.status(400).send("Invalid review ID.");
+  }
 });
 
 module.exports = router;

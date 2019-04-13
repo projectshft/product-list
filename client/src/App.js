bc7b0b3afaccb8dcfe90436032e499b06be9b165
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-04-13 19:49:36 -0400

    creating header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
 import ProductsList from './components/ProductsList'
 import './App.css'
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <ProductsList />
       </div>
     )
---
+++ client/src/components/Header.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Header() {
+  return (
+    <div>
+      <div className="h1 mt-4 mb-5"> PRODUCTS </div>
+      <nav className="navbar navbar-light navbar-expand-lg mt-5 mb-3 py-1">
+        <div className="container-fluid">
+          <form>
+            <div class="form-group form-group-sm">
+              <input
+                type="text"
+                id="search-query"
+                class="form-control input-sm"
+                placeholder="Search"
+              />
+            </div>
+          </form>
+          <div className="dropdown show">
+            <span> Filter by Category: </span>
+            <a
+              className="btn btn-secondary dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Kitchen
+            </a>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a className="dropdown-item" href="#">
+                Sports
+              </a>
+              <a className="dropdown-item" href="#">
+                Garden
+              </a>
+              <a className="dropdown-item" href="#">
+                Entertainment
+              </a>
+            </div>
+          </div>
+          <div className="dropdown show">
+            <span> Sort By: </span>
+            <a
+              className="btn btn-secondary dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Price: Low to High
+            </a>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a className="dropdown-item" href="#">
+                price: High to Low
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}

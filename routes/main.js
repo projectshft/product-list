1307d2840e6da68ff91fd19890535edb7765daf4
dposse <dposse.125@gmail.com>
2019-07-25 16:43:56 -0400

    GET /reviews working with pagination

--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,9 @@ const faker = require('faker')
 const Product = require('../models/product')
 const Review = require('../models/review')
 
-const ITEMS_PER_PAGE = 9;
+const PRODUCTS_PER_PAGE = 9;
+const REVIEWS_PER_PAGE = 40;
 
-//NEED TO ADD RANDOM REVIEWS
 router.get('/generate-fake-data', (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product()
@@ -44,11 +44,11 @@ router.get('/products', (req, res, next) => {
     return res.status(400).send('Invalid page number.');
   }
 
-  const numItemsToSkip = (pageNum - 1) * ITEMS_PER_PAGE;
+  const numItemsToSkip = (pageNum - 1) * PRODUCTS_PER_PAGE;
   
   Product.find()
     .skip(numItemsToSkip)
-    .limit(ITEMS_PER_PAGE)
+    .limit(PRODUCTS_PER_PAGE)
     .exec((err, products) => {
       if (err) throw err;
 
@@ -87,24 +87,30 @@ router.get('/products/:product', (req, res, next) => {
 
 });
 
+//paginate like products
+router.get('/reviews', (req, res, next) => {
+  const pageNum = req.query.page || 1;
+  //validate pageNum - must be >= 1
+  if (pageNum < 1) {
+    return res.status(400).send('Invalid page number.');
+  }
+
+  const numItemsToSkip = (pageNum - 1) * REVIEWS_PER_PAGE;
+  
+  Review.find()
+    .skip(numItemsToSkip)
+    .limit(REVIEWS_PER_PAGE)
+    .exec((err, reviews) => {
+      if (err) throw err;
+      Review.countDocuments((err, count) => {
+        if (err) throw err;
+        res.status(200).send({
+          totalReviews: count,
+          reviewsOnPage: reviews.length,
+          reviews
+        });
+      });
+    });
+});
+
 module.exports = router
-
-
-  //can be deleted, kept for reference to know what is working 
-  // //add a review for testing
-  // Product.findOne()
-  //   .exec((err, product) => {
-  //     const review = new Review({
-  //       userName: 'test',
-  //       text: 'test',
-  //       product: product._id
-  //     });
-
-  //     review.save();
-
-  //     product.reviews.push(review);
-
-  //     product.save((err, result) => {
-  //       console.log(result);
-  //     });
-  //   })

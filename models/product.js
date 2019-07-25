b4499d3b1447c82f2470031ba1a99b7fec424d14
Jake Galligan <galligan1996@gmail.com>
2019-07-25 16:00:16 -0400

    Add review model. Adding a random number of reviews to each product within the generate-random-data endpoint

--- models/product.js
+++ models/product.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Review = require('./review')
+
+
 
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
-  image: String
+  image: String,
+  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,26 @@
 const router = require('express').Router()
 const faker = require('faker')
 const Product = require('../models/product')
+const Review = require('../models/review')
+let randomNumberBetweenOneandTen = Math.floor(Math.random()*(10-1+1)+1);
 
 router.get('/generate-fake-data', (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product()
-
+    //add a random number of reviews to the review array on each product
+    for (let i = 0; i < randomNumberBetweenOneandTen; i++) {
+    let review = new Review()
+    review.userName = faker.internet.userName();
+    review.text = faker.lorem.sentence();
+    review.save();
+    product.reviews.push(review);
+}
+    //add content to each product property
     product.category = faker.commerce.department()
     product.name = faker.commerce.productName()
     product.price = faker.commerce.price()
     product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+    
 
     product.save((err) => {
       if (err) throw err
@@ -30,8 +41,8 @@ router.get('/products', (req, res, next) => {
       .limit(perPage)
       .exec((err, products) => {
         // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
-        Product.count().exec((err, count) => {
-          if (err) return next(err)
+        Product.countDocuments().exec((err, count) => {
+            if (err) return next(err)
   
           res.send(products)
         })

e9e8f9fe2df9930c7d8993999b4d32a1c981c5f7
Jake Galligan <galligan1996@gmail.com>
2019-07-26 14:03:54 -0400

    Adding fetchProducts() to actions/index.js which will get all products from the api with no queries added

---
+++ client/src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+//set up function that loads all posts with no specifications
+export function fetchProducts() {
+    const request = axios.get('localhost:8000/products')
+    return {
+        type: FETCH_PRODUCTS,
+        payload: request
+    }
+}

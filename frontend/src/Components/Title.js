33126e2b12d52f5145da207659f85750496dbc62
dposse <dposse.125@gmail.com>
2019-07-26 15:35:49 -0400

    Created Title component and imported bootstrap

---
+++ frontend/src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => (
+  <div className='container'>
+    <div className='row'>
+      <h1 className='col-12 text-center'>
+        PRODUCTS
+      </h1>
+    </div>
+  </div>
+);
+
+export default Title;

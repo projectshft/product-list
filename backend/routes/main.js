75bfc533c2090b7d82867c40a6edd860df583bc2
Kyle Kimber <kimber.kyle@gmail.com>
2019-07-26 14:59:02 -0400

    Adde the routes for reviews and productID from working with Ronnie yesterday

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -17,28 +17,33 @@ router.get('/generate-fake-data', (req, res, next) => {
     })
   }
   res.end()
-})
-
- router.get('/products', (req, res, next) => {
-  const perPage = 9
-
-  // return the first page by default
-  const page = req.query.page || 1
-
-  Product
-    .find({})
-    .skip((perPage * page) - perPage)
-    .limit(perPage)
-    .exec((err, products) => {
-      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
-      Product.count().exec((err, count) => {
-        if (err) return next(err)
-
-        res.send(products)
-      })
-    })
 });
-
-
-
+// Pagination GET route
+router.get('/products', (request, response, next) => {
+	const itemsPerPage = 9;
+	const pageNumber = request.query.page || 1;
+	let pageSkip = ((pageNumber * itemsPerPage) - itemsPerPage);
+	Product.find().skip(pageSkip).limit(itemsPerPage).exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
+		Product.count().exec((err, count) => {
+			if (err) throw err;
+			response.send(products);
+		})
+	});
+  });
+  // GET route for /products/:productId (Returns a specific product by its id)
+router.get('/products/:productId', (request, response) => {
+	const {productId}  = request.params;
+	Product.find({_id: productId}).exec((err, data) => {
+		if (err) throw err;
+		response.send(data);
+	});
+});
+// GET route for /reviews: (Returns all reviews but limited to 40 at a time.)
+router.get('/reviews', (request, response, next) => {
+	Review.find().exec((err, data) => {
+		if (err) throw err;
+		response.send(data);
+	});
+});
 module.exports = router

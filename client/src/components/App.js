8abb36b92fd12a0f4785f70958ef4311600d84a4
Kate Kremer <katrinakremer@gmail.com>
2019-07-26 11:55:50 -0400

    add browser route to render app component

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p></p>
+    
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
+import App from './components/App'
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 // import reducers from './reducers';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
@@ -11,13 +12,16 @@ import promise from 'redux-promise';
 
 import 'bootstrap/dist/css/bootstrap.css'
 
-const createStoreWithMiddleware = applyMiddleWare(promise)(createStore);
+// const createStoreWithMiddleware = applyMiddleWare(promise)(createStore);
 
 
 ReactDOM.render((
-<Provider store={createStoreWithMiddleware(reducers)}>  
+
   <BrowserRouter>
-    <App />
+    <Switch>
+      <Route exact path='/products' component={App}/>
+      <Redirect from='/' to='/products'/>
+    </Switch>
   </BrowserRouter>
-</Provider>
+
 ), document.getElementById('root'));

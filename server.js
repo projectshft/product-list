59d27a459e8d148b77c86228131d5898c1ad0051
dposse <dposse.125@gmail.com>
2019-07-26 10:20:44 -0400

    Added cors and configured postman and cors whitelist

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost/products', {useNewUrlParser: true});
 
 const app = express();
+
+//only want to allow cors to react dev server
+//make sure to set postman header- Origin: localhost:8000
+const whitelist = ['localhost:8000'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({

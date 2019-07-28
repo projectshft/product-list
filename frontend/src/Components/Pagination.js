db194865c0bbbc02da5d26a6ed36b0c2d0840e0b
dposse <dposse.125@gmail.com>
2019-07-28 11:19:08 -0400

    Created a button to manually test fetchProducts in Pagination

--- frontend/src/Components/Pagination.js
+++ frontend/src/Components/Pagination.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Pagination = () => (
-  <div>
-    placeholder
-  </div>
-);
+const Pagination = ({ fetchProducts }) => {
+  const handleClick = () => {
+    fetchProducts({ page: 4 });
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>goto page 4</button>
+    </div>
+  );
+};
+
 
 export default Pagination;

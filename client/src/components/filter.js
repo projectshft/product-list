b443c928b67b5648d3d9a0c0609461f2b0402db8
Jake Galligan <galligan1996@gmail.com>
2019-07-28 09:43:33 -0400

    refactor categories.js to map through an array of listed projects and create a button for each instead of hardcoding in buttons for each category manually

--- client/src/components/filter.js
+++ client/src/components/filter.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { addCategory, fetchProducts} from '../actions';
 
+let categories = ['Games', 'Baby', 'Computers', 'Home', 'Industrial', 'Toys', 'Electronics',
+                'Health', 'Grocery', 'Tools', 'Music', 'Shoes', 'Jewelery']
 
 class Filter extends Component {
     
@@ -15,22 +17,23 @@ class Filter extends Component {
         this.props.fetchProducts(1, category, sort)
     }
 
+    renderCategories () {
+        return categories.map(category => {
+            return (
+                <button className='btn btn-primary' value= {category} onClick={this.handleClick.bind(this)}>
+                    {category}
+                </button>
+            )
+
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Categories: </h1>
-                <button className='btn btn-primary' value= 'Home' onClick={this.handleClick.bind(this)}>
-                    Home
-                </button>
-                <button className='btn btn-primary' value= 'Industrial' onClick={this.handleClick.bind(this)}>
-                    Industrial
-                </button>            
-                <button className='btn btn-primary' value= 'Games' onClick={this.handleClick.bind(this)}>
-                    Games
-                </button>
-                <button className='btn btn-primary' value= 'Toys' onClick={this.handleClick.bind(this)}>
-                    Toys
-                </button>
+                {this.renderCategories()}
+              
             </div>
        
         )

9cf119969985d8cd34fec92a38f554db1b6fb214
Kyle Kimber <kimber.kyle@gmail.com>
2019-08-11 20:12:34 -0400

    Got the sort and filter functionality to work. Also finished the delete routes

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -36,20 +36,37 @@ router.get('/generate-fake-data', (req, res, next) => {
 // Pagination GET route
 router.get('/products', (request, response, next) => {
 	const itemsPerPage = 9;
-	const pageNumber = request.query.page || 1;
-	let pageSkip = ((pageNumber * itemsPerPage) - itemsPerPage);
-	Product.find().skip(pageSkip).limit(itemsPerPage).exec((err, products) => {
+  const pageNumber = request.query.page || 1;
+  const categoryTheClientSent = request.query.category ;
+  const priceOrderTheClientSent = request.query.price;
+  const searchQuery = {};
+  const sortQuery = {};
+  
+  if(categoryTheClientSent !== undefined) {
+    searchQuery.category = categoryTheClientSent;
+  }
+  if(priceOrderTheClientSent!== undefined) {
+    if(priceOrderTheClientSent == "lowest") {
+      sortQuery.price = 'asc'
+    }
+    else if (priceOrderTheClientSent == "highest") {
+      sortQuery.price = "desc"
+    }
+  }
+  let pageSkip = ((pageNumber * itemsPerPage) - itemsPerPage);
+  Product.find(searchQuery).sort(sortQuery).skip(pageSkip).limit(itemsPerPage).exec((err, products) => {
       // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
 		Product.count().exec((err, count) => {
-			if (err) throw err;
-			response.send(products);
+      if (err) throw err;
+      response.send(products);
+      
 		})
 	});
 });
   // GET route for /products/:productId (Returns a specific product by its id)
 router.get('/products/:productId', (request, response) => {
 	const {productId}  = request.params;
-	Product.find({_id: productId}).exec((err, data) => {
+	Product.findOne({_id: productId}).exec((err, data) => {
 		if (err) throw err;
 		response.send(data);
 	});
@@ -62,12 +79,66 @@ router.get('/reviews', (request, response, next) => {
 	});
 });
 
-// router.post("/products", (request, response) => {
-//   const { product } = request.params;
-//   // const product = goals.find(goal => goal.id == goalId);
-// //add the new product to the product document using save();
-//   user.acceptedGoals.push(goal);
-//   saveCurrentUser(user);
-//   return response.end();
-// });
+router.post("/products", (request, response) => {
+  const { category, name, price } = request.body.data 
+  //using Product schema to create a new document in MONGODB
+  let product = new Product()
+  product.category = category
+  product.name = name
+  product.price = price
+  product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+  product.reviews = []
+  product.save((err) => {
+    if (err) throw err
+    return response.send(product);
+  })
+  //response confirms the product was posted. 
+  
+});
+
+router.post("/:product/reviews", (request, response) => {
+  const {product}  = request.params;
+
+  const { username, text } = request.body.data 
+  //using Review schema to create a new document in MONGODB
+  let review = new Review()
+  review.username = username
+  review.text = text
+  review.product = product
+  review.save((err) => {
+    if (err) throw err
+  })
+	Product.findOne({_id: product}).exec((err, foundProduct) => {
+    if (err) throw err;
+    foundProduct.reviews.push(review._id)
+    foundProduct.save((err, savedProduct) => {
+      if (err) throw err
+      // console.log(savedProduct)  
+      //response confirms the product was posted. 
+      return response.send(review);
+    })
+  });
+
+});
+
+// GET route for /products/:productId (Returns a specific product by its id)
+router.delete('/products/:productId', (request, response) => {
+  const {productId}  = request.params;
+  Product.findOne({_id: productId}).remove().exec((err, data) => {
+    if (err) throw err;
+    response.send(data);
+  });
+});
+//this route deletes the review doc from reviews collection, but not the review in the review array in the product. 
+router.delete("/reviews/:reviewId", (request, response) => {
+  const {reviewId}  = request.params;
+  console.log(reviewId);
+  Review.findOne({_id: reviewId}).remove().exec((err, data) => {
+    if (err) throw err;
+    response.send(data);
+  });
+});
+
+
+
 module.exports = router

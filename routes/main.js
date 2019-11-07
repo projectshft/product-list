b5eca694f0645a5cbb004f2437ceb80f4ab522e3
Tyler <laorik1@live.com>
2019-11-07 14:01:32 -0500

    added initial faker creation and models for product and review with dummy data

---
+++ routes/main.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const faker = require("faker");
+const Product = require("../models/product");
+const Review = require("../models/review");
+
+router.get("/generate-fake-data", (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product();
+
+    product.category = faker.commerce.department();
+    product.name = faker.commerce.productName();
+    product.price = faker.commerce.price();
+    product.image =
+      "https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png";
+
+    product.save(err => {
+      if (err) throw err;
+    });
+
+    for (let j = 0; j < 10; j++) {
+      let review = new Review();
+      review.userName = faker.internet.userName();
+      review.text = faker.lorem.sentences();
+      review.product = product._id;
+
+      review.save(err => {
+        if (err) throw err;
+      });
+
+      product.reviews.push(review._id);
+    }
+  }
+  res.end();
+});
+
+router.get("/products", (req, res, next) => {
+  const perPage = 10;
+
+  // return the first page by default
+  const page = req.query.page || 1;
+
+  Product.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+      });
+    });
+});
+
+module.exports = router;

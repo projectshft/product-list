015a7de2d20ebd8eb631ff803edfe43d0af87770
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-08 11:39:16 -0500

    added get reviews and post for both

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const ReviewSchema = new Schema({
   username: String,
   text: String,
-  product: [{ type: Schema.Types.ObjectId, ref: 'product' }]
+ 
 })
 
 // let review = new Review({
--- routes/main.js
+++ routes/main.js
@@ -7,31 +7,45 @@ router.get('/generate-fake-data', (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product()
 
-//populate reviews with same template as products
-    let review = new Review()
 
     product.category = faker.commerce.department()
     product.name = faker.commerce.productName()
     product.price = faker.commerce.price()
     product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
 
+   
+
+// populate reviews with same template as products
+    let review = new Review()
+
+    review.userName = faker.internet.userName()
+    review.text = faker.lorem.text()
+    review.save();
+    product.reviews.push(review);
+    
     product.save((err) => {
       if (err) throw err
     })
   }
+
   res.end()
 })
 
 router.get('/products', (req, res, next) => {
     const perPage = 9
-  
+    const category = req.query.category
+    var searchedCategory = {};
+    if (category) {
+      searchedCategory = {category: category}
+    }
     // return the first page by default
     const page = req.query.page || 1
   
     Product
-      .find({})
+      .find(searchedCategory)
       .skip((perPage * page) - perPage)
       .limit(perPage)
+      .populate('reviews', '-_id')
       .exec((err, products) => {
         // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
         Product.count().exec((err, count) => {
@@ -40,8 +54,94 @@ router.get('/products', (req, res, next) => {
           res.send(products)
         })
       })
+
+
+
+router.get('/reviews', (req, res, next) => {
+  
+    const page = req.query.page || 1
+  
+    Review
+      .find({})
+      .skip((perPage * page) - perPage)
+      .limit(perPage)
+      .exec((err, review) => {
+        // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
+        Review.count().exec((err, count) => {
+          if (err) return next(err)
+  
+          res.send(review);
+        })
+      })
   })
 
+router.get('/products/:product', (req, res, next) => {
   
+    const idforProduct = req.params.product;
   
+    Product
+      .find({_id : idforProduct})
+      .limit(perPage)
+      .exec((err, product) => {
+        // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back
+        Product.count().exec((err, count) => {
+          if (err) return next(err)
+  
+          res.send(product)
+        })
+      })
+    })
+  })
+  
+router.post('/products', (req, res, next) => {
+    
+    const postProduct = req.body;
+    if (!postProduct.category || !postProduct.name || !postProduct.price || !postProduct.image) {
+      //error if the 4 required params are not entered in 
+        res.writeHead(400, 'Please enter valid params');
+        res.end();
+    }
+  //creating new instance of product
+    let newProduct = new Product();
+    newProduct.category =  postProduct.category;
+    newProduct.name = postProduct.name;
+    newProduct.price =  postProduct.price;
+    newProduct.image =  postProduct.image;
+    newProduct.reviews = [];
+
+
+    newProduct.save((err) => {
+        if (err) throw err
+      })
+    res.end();
+
+})
+
+router.post('/products/reviews', (req, res, next) => {
+  const idforProduct = req.params.product
+
+  const postReview = req.body;
+  if (!postReview.username || !postProduct.text) {
+    //error if the 2 required params are not entered in 
+      res.writeHead(400, 'Please enter valid params');
+      res.end();
+  }
+//creating new instance of review
+  let newReview = new Review();
+  newReview.username =  postReview.username;
+  newReview.text = postReview.text;
+  NewReview.save();
+
+
+  Product.findById(idforProduct, (err, product) => {
+    if (err) throw err;
+    product.reviews.push(newReview);
+    product.save(err => {
+        if (err) throw err
+        res.end();
+    })
+})
+})
+
+
 module.exports = router

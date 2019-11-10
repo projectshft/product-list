d731c476a71f4e4af6949b631c3cc09d3bb7140f
Tyler <laorik1@live.com>
2019-11-10 17:21:38 -0500

    implemented cors, that was easy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 mongoose.connect("mongodb://localhost/products", { useNewUrlParser: true });
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost/products", { useNewUrlParser: true });
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true

40b6e9bfb9d0461ba3cdfe35d3e2b62e64b45870
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-11-11 20:49:39 -0500

    Add frustrated comments to pagination code

--- src/components/product-index.js
+++ src/components/product-index.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 import { fetchProducts } from '../actions'
 
 class ProductIndex extends Component {
+
   componentDidMount() {
     this.props.fetchProducts(1);
   }
@@ -27,6 +28,21 @@ class ProductIndex extends Component {
     })
   }
 
+  // NOTE THIS DOESN'T WORK
+  // After hours of debugging and googling I've no idea why I can't pull this from Redux store/state
+  // totalProducts is empty... but using redux devtools I'm pointing at the right spot. WHY tho
+  renderPagination() {
+    // Determine number of pages needed based on num of products in database
+    let totalProducts = this.props.products.productCount
+    // Rounds up for last page of products doesn't cut off
+    let pagesNeeded = Math.ceil(totalProducts/9)
+    for (let i = 1; i <= pagesNeeded; i++) {
+      return (
+        <li className="page-item"><a className="page-link" href="#">{i}</a></li>
+        )
+      }
+    }
+
   render() {
     return (
       <div>
@@ -34,19 +50,19 @@ class ProductIndex extends Component {
           <div className="col-md-9 offset-md-2">
             <div className='page-header text-center'>
               <h1>Hello from Product Index!</h1>
-              <h3>Product Count: {this.props.products.productCount}</h3>
+              <h3>Products: placeholder</h3>
             </div>
             <div className='products-display'>
               {this.renderProducts()}
             </div>
-            
           </div>
         </div>
         <nav aria-label="Page navigation example">
           <ul className="pagination">
-            <li className="page-item"><a className="page-link" href="#">1</a></li>
+            {this.renderPagination()}
+            {/* <li className="page-item"><a className="page-link" href="#">1</a></li>
             <li className="page-item"><a className="page-link" onClick={this.onPageClick} href="#">2</a></li>
-            <li className="page-item"><a className="page-link" href="#">3</a></li>
+            <li className="page-item"><a className="page-link" href="#">3</a></li> */}
           </ul>
         </nav>
       </div>

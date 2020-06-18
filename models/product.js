2292222ed97af4f43faebc0b23d0c6ccc845fcca
Joe Krall <kralljr@outlook.com>
2020-06-18 16:21:00 -0400

    working route: GET /products

--- models/product.js
+++ models/product.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const ReviewSchema = new Schema({
+  userName: String,
+  text: String,
+})
+
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
-  image: String
+  image: String,
+  reviews: [ReviewSchema]
 })
 
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('Product', ProductSchema) 

2c813389c73326534355fd21d94701f247f321c8
Joe Krall <kralljr@outlook.com>
2020-06-18 23:14:11 -0400

    reset database with empty reviews arrays

--- routes/main.js
+++ routes/main.js
@@ -2,21 +2,22 @@ const router = require('express').Router()
 const faker = require('faker')
 const Product = require('../models/product')
 
-// router.get('/generate-fake-data', (req, res, next) => {
-//   for (let i = 0; i < 90; i++) {
-//     let product = new Product()
+router.get('/generate-fake-data', (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product()
 
-//     product.category = faker.commerce.department()
-//     product.name = faker.commerce.productName()
-//     product.price = faker.commerce.price()
-//     product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+    product.category = faker.commerce.department()
+    product.name = faker.commerce.productName()
+    product.price = faker.commerce.price()
+    product.image = 'https://www.oysterdiving.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+    product.reviews = [];
 
-//     product.save((err) => {
-//       if (err) throw err
-//     })
-//   }
-//   res.end()
-// })
+    product.save((err) => {
+      if (err) throw err
+    })
+  }
+  res.end()
+})
 
 router.param('product', function(req, res, next, id) {
   req.product = Product.find({_id: id});

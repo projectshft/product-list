1530b2edaeb3aef05abc02ed540ad736f305c88a
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-06-19 12:50:52 -0400

    fixed review.js

--- models/product.js
+++ models/product.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Review = require('../models/review')
-
 const ProductSchema = new Schema({
   category: String,
   name: String,
--- models/review.js
+++ models/review.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Review = require('../models/review')
-
-const ProductSchema = new Schema({
-  category: String,
-  name: String,
-  price: Number,
-  image: String,
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'review' }]
+const ReviewSchema = new Schema({
+  userName: String,
+  text: String,
+  product: { type: Schema.Types.ObjectId, ref: 'product' }
 })
 
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('Review', ReviewSchema)

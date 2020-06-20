6ca024f491254a214050bbe275e7550f8fcae309
ADH <61984231+adholton@users.noreply.github.com>
2020-06-20 18:49:01 -0400

    Adds event handlers, mapDispatchToProps and connect to search-filter-sort-bar.js

--- app/src/containers/search-filter-sort-bar.js
+++ app/src/containers/search-filter-sort-bar.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Component } from 'react';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchProducts } from '../actions/index';
 
 //dropdowns can be a button if needed
 /*another option if needed:
@@ -17,12 +18,56 @@ Be sure to explore our custom forms to further style <select>s.
 
 Where do we make our row? another outer div with class="row"?
  */
-export default class SearchFilterSortBar extends Component {
+class SearchFilterSortBar extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    //the props is an object that just contains the fetchProducts function
+
+    //the state will contain our user search input, category selection and/or price sort selection
+    this.state = { search: '', category: '', price: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
+    this.onCategorySelect = this.onCategorySelect.bind(this);
+    this.onPriceSortSelect = this.onPriceSortSelect.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ search: event.target.value });
+  }
+
+  onSearchButtonClick(event) {
+    event.preventDefault();
+
+    // This will fetch the products from the search input
+    // the state at this point is already updated from event handler of the user typing, so we invoke the fetchProducts function (to make the api call) and then reset the state back back to its original state (clearing the search property value)
+    this.props.fetchProducts(this.state.search);
+    this.setState({ term: '' });
+  }
+
+  //this will update the state if the user selects a category. Then the fetchProducts function will make the api call for the selected category's products
+  // look up how to know when user selects an option..
+  onCategorySelect(event) {
+    this.setState({ category: event.target.value });
+    this.props.fetchProducts(this.state.category);
+  }
+
+  // this will update the state if the user selects a price sort option (highest to lowest, and vice versa)
+  // since we have query parameters for price sorting, I'm thinking this will also make an api call based on the user's selection
+  // research the way our handler will be invoked, is it onChange? where will I put the handler in the jsx below?
+  onPriceSortSelect(event) {
+    this.setState({ price: event.target.value });
+    this.props.fetchProducts(this.state.price);
+  }
+
+
   render() {
     return (
       <form className="form-inline">
         <label className="ml-2 mr-2" htmlFor="exampleFormControlInput1">Search</label>
-        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Search..." />
+        <input value={this.state.search} onChange={this.onInputChange} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Search..." />
+        <button onClick={this.onSearchButtonClick} type="submit" className="btn btm-primary">Search</button>
         <label className="ml-2 mr-2" htmlFor="inlineFormCustomSelectCategory">Filter by Category: </label>
         <select className="form-control custom-select" id="inlineFormCustomSelectCategory">
           <option defaultValue="All">All</option>
@@ -59,3 +104,14 @@ export default class SearchFilterSortBar extends Component {
     );
   }
 }
+
+// the only action we're binding is fetchProducts. This function is wrapped in a dispatch call and will dispatch an action to our reducer
+// we're invoking the fetchProducts action creator every time the user submits the search input, chooses a category or chooses a price sort. 
+// this is the component is accessing the fetchProducts prop
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchProducts }, dispatch);
+}
+
+// We pass in null (placeholder) as the first connect argument because we're only dispatching from this component.
+// There is no mapStateToProps because we're not displaying any data in this component
+export default connect(null, mapDispatchToProps)(SearchFilterSortBar);

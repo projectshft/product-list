1d45c890a28a58afc5bb105194fc3e8d5b5ee48e
ADH <61984231+adholton@users.noreply.github.com>
2020-06-20 17:27:42 -0400

    Adds all hardcoded categories

--- app/src/index.js
+++ app/src/index.js
@@ -60,10 +60,29 @@ class SearchFilterSortBar extends React.Component {
         <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Search..." />
         <label className="ml-2 mr-2" htmlFor="inlineFormCustomSelectCategory">Filter by Category: </label>
         <select className="form-control custom-select" id="inlineFormCustomSelectCategory">
-          {/* <option defaultValue="Toys">Toys</option> */}
+          <option defaultValue="All">All</option>
+          <option value="Home">Baby</option>
+          <option value="Baby">Books</option>
+          <option value="Garden">Grocery</option>
+          <option value="Home">Clothing</option>
+          <option value="Baby">Tools</option>
+          <option value="Garden">Kids</option>
           <option value="Home">Home</option>
-          <option value="Baby">Baby</option>
-          <option value="Garden">Garden</option>
+          <option value="Baby">Garden</option>
+          <option value="Garden">Automotive</option>
+          <option value="Home">Toys</option>
+          <option value="Baby">Beauty</option>
+          <option value="Garden">Computers</option>
+          <option value="Home">Sports</option>
+          <option value="Baby">Shoes</option>
+          <option value="Garden">Games</option>
+          <option value="Home">Jewelry</option>
+          <option value="Baby">Electronics</option>
+          <option value="Garden">Industrial</option>
+          <option value="Garden">Movies</option>
+          <option value="Home">Music</option>
+          <option value="Baby">Health</option>
+          <option value="Garden">Outdoors</option>
         </select>
         <label className="ml-2 mr-2" htmlFor="inlineFormCustomSelectSort">Sort by Price: </label>
         <select className="form-control custom-select" htmlFor="inlineFormCustomSelectSort">

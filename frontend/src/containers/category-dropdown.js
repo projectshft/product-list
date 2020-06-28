60c45f6e1b1bf3fb17ad1201c00ea2eb1188aa37
Joe Krall <kralljr@outlook.com>
2020-06-28 17:43:06 -0400

    category-dropdown working with hardcoded data

--- frontend/src/containers/category-dropdown.js
+++ frontend/src/containers/category-dropdown.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchProducts, setCategory } from '../actions/index';
-import axios from 'axios';
 
 class CategoryDropdown extends Component {
   constructor(props) {
@@ -10,48 +9,69 @@ class CategoryDropdown extends Component {
 
     // The categories have local state for now
     this.state = {
-      allCategories: [] // default sort param
+      categories: [
+        "Automotive",
+        "Baby",
+        "Beauty",
+        "Books",
+        "Clothing",
+        "Computers",
+        "Electronics",
+        "Games",
+        "Garden",
+        "Grocery",
+        "Health",
+        "Home",
+        "Industrial",
+        "Jewelery",
+        "Kids",
+        "Movies",
+        "Music",
+        "Outdoors",
+        "Shoes",
+        "Sports",
+        "Tools",
+        "Toys"
+      ]
     }
+
     this.selectCategoryFromMenu = this.selectCategoryFromMenu.bind(this);
   }
 
-  componentDidMount() {
+//   componentDidMount() {
 
-      let url = 'http://localhost:8000/categories';
+//       let url = 'http://localhost:8000/categories';
     
-      const request = axios.get(url);
-      console.log("This is the categories request");
-      console.log(request);
-      this.setState({allCategories: request.data});
- }
+//       const request = axios.get(url);
+//       console.log("This is the categories request");
+//       console.log(request);
+//       this.setState({allCategories: request.data});
+//  }
 
-  selectCategoryFromMenu (category, event) {
+  selectCategoryFromMenu = (category, event) => {
     event.preventDefault();
 
     this.props.setCategory(category);
     this.props.fetchProducts(this.props.searchTerm, this.props.sort, category)
   }
 
-  renderCategories(categoryData) {
-    
-    return (
-      <button className="dropdown-item" type="button" 
-        onClick={event => this.selectCategoryFromMenu(categoryData, event)}>
-        {categoryData}
-      </button>
-    );
-  }
 
 
   render() {
-    console.log(this.state.allCategories);
     return (
       <div>
         <div className="dropdown"><button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
             Set Category
           </button>
           <div className="dropdown-menu">
-            {this.state.allCategories.map(this.renderCategories)}
+            {this.state.categories.map((categoryData) => {
+                  return (
+                    <button className="dropdown-item" type="button" 
+                      onClick={event => this.selectCategoryFromMenu(categoryData, event)}>
+                      {categoryData}
+                    </button>
+                  );
+            })}
           </div>
         </div>
     </div>

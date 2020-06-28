86437dd33b532474e30a0b5bb046cfdf4aade542
Joe Krall <kralljr@outlook.com>
2020-06-27 23:24:38 -0400

    changed state defaults in reducers. sort, category now have empty object as default

--- frontend/src/reducers/reducer-search-term.js
+++ frontend/src/reducers/reducer-search-term.js
@@ -1,6 +1,6 @@
 import { SET_SEARCH_TERM } from '../actions/index';
 
-export default function(state = {}, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return action.payload; // data .product, data .count

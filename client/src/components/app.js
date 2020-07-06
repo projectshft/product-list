9387599f8226c1d594f5eccce29c62c75a74621e
Clyde Weed <clydeweed@gmail.com>
2020-07-06 07:27:06 -0400

    Adding App component

---
+++ client/src/components/app.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Products from "../containers/products.js"
+import Categories from "../containers/categories.js"
+import Sort from "../containers/sort.js"
+import Pages from "../containers/pages.js"
+import SearchBar from '../containers/searchbar.js'
+
+
+class App extends Component {
+  render() {
+   
+    return (
+
+      <div>
+        <div>
+          <Products/>
+          <Categories/>
+          <SearchBar/>
+          <Sort/>
+          <Pages/>
+        </div>
+      </div>
+
+    )
+  }
+};
+
+
+export default App;

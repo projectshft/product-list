3c44d25c844d863ec1ae5f5e6da8a75040b7222c
Clyde Weed <clydeweed@gmail.com>
2020-07-06 07:29:33 -0400

    Adding Routes

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const faker = require('faker')
 const Product = require('../models/product')
+const ProductReview = require('../models/productreview')
 
 
 router.get('/generate-fake-data', (req, res, next) => {
@@ -39,6 +40,28 @@ router.get('/products', (req, res, next) => {
   })
 })
 
+//GET /products/:product: Returns a specific product by its id
+
+
+
+
+//GET /products/:product/reviews: Returns ALL the reviews for a product, but limited to 4 at a time. This one will be a little tricky as you'll have to retrieve them out of the products. You should be able to pass in an optional page query parameter to paginate.
+
+
+
+//POST /products: Creates a new product in the database
+
+
+
+//POST /products/:product/reviews: Creates a new review in the database by adding it to the correct product's reviews array.
+
+
+
+//DELETE /products/:product: Deletes a product by id
+
+
+
+//DELETE /reviews/:review: Deletes a review by id
 
 
 module.exports = router

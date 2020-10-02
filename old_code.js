9eb19809ad47ce12769eddb64f0e1fb3abf598bb
PJ Foley <pjfoley@gmail.com>
2020-10-02 11:35:43 -0700

    basic functionality of endpoints implemented

--- old_code.js
+++ old_code.js
@@ -23,3 +23,5 @@ router.get('/newroute', (req, res, next) => {
 })
 // example product
 // {"_id":{"$oid":"5f7616c77b108078742fc9cf"},"category":"Kids","name":"Intelligent Fresh Chair","price":660,"image":"https://via.placeholder.com/250?text=Product+Image","__v":0}
+// {"_id":{"$oid":"5f7616c77b108078742fc9b7"},"category":"Clothing","name":"Practical Fresh Ball","price":22,"image":"https://via.placeholder.com/250?text=Product+Image","__v":0}
+// {"_id":{"$oid":"5f7616c77b108078742fc9bc"},"category":"Health","name":"Handmade Fresh Soap","price":543,"image":"https://via.placeholder.com/250?text=Product+Image","__v":0}
--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ const Product = require('../models/product')
 const Review = require('../models/review')
 
 router.get('/generate-fake-data', (req, res, next) => {
-  for (let i = 0; i < 90; i++) {
+  for (let i = 0; i < 10; i++) {
     let product = new Product()
 
     product.category = faker.commerce.department()
@@ -26,7 +26,7 @@ router.get('/generate-fake-data', (req, res, next) => {
       // 4) ?/&query= search results product name, make case insensitive if possible
 // count of products returned needs to dynamically reflect results
 router.get('/products', (req, res, next) => {
-  const perPage = 10
+  const perPage = 9
 
   // return the first page by default
   const page = req.query.page || 1
@@ -41,7 +41,7 @@ router.get('/products', (req, res, next) => {
       Product.countDocuments().exec((err, count) => {
         if (err) return next(err)
 
-        res.send(products)
+        res.send(JSON.stringify(products))
       })
     })
 })
@@ -51,19 +51,31 @@ router.get('/products/:product', (req, res, next) => {
   Product
     .find({_id: req.params.product})
      .exec((err, products) => {
-  if (err) return next(err)
+
   console.log(products)
-  res.send(products)
+  res.send(JSON.stringify(products))
 })})
 
 // returns all reviews of a product, limited to 4 @ time, ?page= option to start page
 router.get('/products/:product/reviews', (req, res, next) => {
-  if (err) return next(err)
-  res.send(/*response*/)
+  console.log(`product review retrieval for ${req.params.product}`)
+  const perPage = 4
+
+  // return the first page by default
+  const page = req.query.page || 1
+  Review
+    .find({product : req.params.product})
+    .skip((perPage * page) - perPage)
+    .limit(perPage)
+    .exec((err, reviews) => {
+      if (err) console.log(err)
+      console.log(JSON.stringify(reviews))})
+    res.sendStatus(200)
 })
 
-// creates a new product in the database body category / name / price / image
+// creates a new product in the database body = category / name / price / image
 router.post('/products', (req, res, next) => {
+
   console.log(req.body[0].name)
   console.log(req.body[0].price)
   const newProduct = new Product({
@@ -72,34 +84,44 @@ router.post('/products', (req, res, next) => {
     price : req.body[0].price,
     image: req.body[0].image
   })
-  newProduct.save()
-  
-  /* {
-    category : req.body[0].category,
-    name : req.body[0].name,
-    price : req.body[0].price,
-    image: req.body[0].image
-  } */
-  if (err) return next(err)
-  res.send(/*response*/)
+  newProduct.save((err, data) => {
+    if (err) { 
+      console.log(error)
+    } else {
+      console.log('new product successfully created')
+      res.send(200)
+    }
+  })
 })
-
 // creates a new review in the database by adding to array
 router.post('/:product/reviews', (req, res, next) => {
-  if (err) return next(err)
-  res.send(/*response*/)
+  console.log(req.body[0].userName)
+  console.log(req.body[0].text)
+  console.log(req.params.product)
+  const newReview = new Review({
+    userName: req.body[0].userName,
+    text: req.body[0].text,
+    product: req.params.product})
+  newReview.save()
+  res.send(200)
 })
 
 // deletes a product by id
 router.delete('/products/:product', (req, res, next) => {
-  if (err) return next(err)
-  res.send(/*response*/)
+  console.log('deleting', req.params.product)
+  Product.deleteOne({ _id : req.params.product}, (err) => {
+    if (err) console.log(err)
+  })
+  res.sendStatus(200)
 })
 
 // deletes a review by id
 router.delete('/reviews/:review', (req, res, next) => {
-  if (err) return next(err)
-  res.send(/*response*/)
+  console.log('deleting', req.params.review)
+  Review.deleteOne({ _id : req.params.review}, (err) => {
+    if (err) console.log(err)
+  })
+  res.sendStatus(200)
 })
-module.exports = router
 
+module.exports = router

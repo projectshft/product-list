fe9fad1682a7e4bc32aa4bf240cbbfffc0b09c73
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-03 16:38:37 -0400

    adds front-end folder and initial setup

---
+++ front-end/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+

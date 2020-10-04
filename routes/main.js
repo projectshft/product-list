c03a653bca89ef1ec892230df5b82ddc29264a90
Aissa Feldmann <aissaf@umich.edu>
2020-10-03 22:59:54 -0400

    key server operations for /products are functional

--- routes/main.js
+++ routes/main.js
@@ -45,25 +45,131 @@ router.get('/generate-fake-reviews', (request, response, next) => {
     });
 });
 
-//GET Products and Products Count for page in query string, return 9 per page
+//GET Products and Products Count for page in query string, return 9 per page: successful
+//GET Products by category, 9 per page, count of items in category & total count: successful
+//GET All Products sort by price low to high w/ counts: successful
+//GET Products by category as above, sort by price low to high w/ all counts: successful
 router.get('/products', (request, response, next) => {
   const perPage = 9;
   const page = request.query.page || 1; //req.query.page is optional. will default to 1 if not sent
-  Product.find({}) //no filter set with {}; not {name: sponge}, eg
-    .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
-    .limit(perPage) //limit to 9 per page
-    .exec((error, products) => {
-      //products is our 9 results for page 1
-      Product.count() //count all products in entire Products collection
-        .exec((err, productCount) => {
-          if (err) return next(err);
-          const results = {
-            listOfProducts: products,
-            countOfAllProducts: productCount,
-          };
-          response.send(results); //
-        });
-    });
+
+  if (!request.query.category) {
+    Product.find({}) //
+      .count()
+      .exec((error, allProductsCount) => {
+        if (error) return next(error);
+
+        if (!request.query.sort) {
+          Product.find({})
+            .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+            .limit(perPage) //limit to 9 per page
+            .exec((error, allProductsUnsorted) => {
+              if (error) return next(error);
+              const allProdsUnsorted = {
+                countOfAllProducts: allProductsCount,
+                listOfAllProducts: allProductsUnsorted,
+              };
+              response.send({ allProdsUnsorted: allProdsUnsorted });
+            });
+        } else if (request.query.sort === 'lowToHigh') {
+          Product.find({})
+            .sort({ price: +1 })
+            .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+            .limit(perPage) //limit to 9 per page
+            .exec((error, allProductsLowToHigh) => {
+              if (error) return next(error);
+              const allProdsLowToHigh = {
+                countOfAllProducts: allProductsCount,
+                allProductsLowToHigh: allProductsLowToHigh,
+              };
+              response.send({
+                allProdsLowToHigh: allProdsLowToHigh,
+              });
+            });
+        } else if (request.query.sort === 'highToLow') {
+          Product.find({})
+            .sort({ price: -1 })
+            .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+            .limit(perPage) //limit to 9 per page
+            .exec((error, allProductsHighToLow) => {
+              if (error) return error;
+              const allProdsHighToLow = {
+                countOfAllProducts: allProductsCount,
+                allProductsHighToLow: allProductsHighToLow,
+              };
+              response.send({
+                allProdsHighToLow: allProdsHighToLow,
+              });
+            });
+        }
+      });
+  } else {
+    Product.find({})
+      .count()
+      .exec((error, allProductsCount) => {
+        if (error) return next(error);
+
+        Product.find({ category: request.query.category })
+          .count()
+          .exec((error, countOfProdInCategory) => {
+            if (error) {
+              return error;
+            }
+            console.log('no error!');
+            const prodCatCount = countOfProdInCategory;
+
+            if (!request.query.sort) {
+              Product.find({ category: request.query.category })
+                .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+                .limit(perPage) //limit to 9 per page
+                .exec((error, listOfProdInCategoryUnsorted) => {
+                  if (error) return next(error);
+                  const productCatResultsUnsorted = {
+                    allProductsCount: allProductsCount,
+                    countOfProdInCategory: prodCatCount,
+                    listOfProdInCategoryUnsorted: listOfProdInCategoryUnsorted,
+                  };
+                  response.send({
+                    productCatResultsUnsorted: productCatResultsUnsorted,
+                  });
+                });
+            } else if (request.query.sort === 'lowToHigh') {
+              Product.find({ category: request.query.category })
+                .sort({ price: +1 })
+                .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+                .limit(perPage) //limit to 9 per page
+                .exec((error, prodByCatLowToHigh) => {
+                  if (error) return next(error);
+                  const prodCatResultsLowToHigh = {
+                    allProductsCount: allProductsCount,
+                    countOfProdInCategory: countOfProdInCategory,
+                    prodByCatLowToHigh: prodByCatLowToHigh,
+                  };
+                  response.send({
+                    prodCatResultsLowToHigh: prodCatResultsLowToHigh,
+                  });
+                });
+            } else if (request.query.sort === 'highToLow') {
+              Product.find({ category: request.query.category })
+                .sort({ price: -1 })
+                .skip(perPage * page - perPage) //9*1=9-9=0: skip nothing for page 1
+                .limit(perPage) //limit to 9 per page
+                .exec((error, prodByCatHighToLow) => {
+                  if (error) return next(error);
+                  const productCatResultsHighToLow = {
+                    allProductsCount: allProductsCount,
+                    countOfProdInCategory: countOfProdInCategory,
+                    prodByCatHighToLow: prodByCatHighToLow,
+                  };
+                  response.send({
+                    productCatResultsHighToLow: productCatResultsHighToLow,
+                  });
+                  response.end();
+                });
+            }
+          });
+      });
+  }
 });
 
 //GET Product by productId: successful
@@ -110,7 +216,6 @@ router.get('/products/:productId/reviews', (request, response, next) => {
       //   }
       //   response.end();
     });
-  
 });
 
 //COULD DO: Handle edge cases and errors
@@ -146,6 +251,7 @@ router.post('/products/:productId/reviews', (request, response, next) => {
       let review = new Review();
       review.userName = request.body.userName;
       review.text = request.body.text;
+      review.productId = request.params.productId;
       review.save((error) => {
         if (error) return next(error);
         //push the review into the product's reviews array and save the product
@@ -174,23 +280,25 @@ router.delete('/products/:productId', (request, response, next) => {
   });
 });
 
-
-//TODO!
-//DELETE review by reviewID
-router.get('/reviews/:reviewId', (request, response, next) => {
+//TODO! remove reviewId from the product
+//DELETE review by reviewID: successful
+router.delete('/reviews/:reviewId', (request, response, next) => {
   //if the reviewId is invalid, deliver a Bad Request error
   if (!mongoose.isValidObjectId(request.params.reviewId)) {
     response.sendStatus(400);
     response.end();
   }
-
-  // Review.find({}, (error, reviews) => {
-  //   if (error) {
-  //     response.sendStatus(404);
-  //   } else {
-  //     response.send(reviews);
-  //   }
-  // });
+  Review.deleteOne({ _id: request.params.reviewId }, (error, review) => {
+    if (error) {
+      response.sendStatus(404);
+    } else {
+      Product.find({ reviews: request.params.reviewId }, (error, product) => {
+        if (error) return error;
+        console.log('found');
+      });
+    }
+    response.end();
+  });
 });
 
 module.exports = router;
--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Component } from 'react';
-// import { fetchDestination } from '../actions';
-// import { sendDistance } from '../actions';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';

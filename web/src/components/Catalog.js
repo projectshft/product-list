c7a953db01673765a0c806f2fe6f283148156963
PJ Foley <pjfoley@gmail.com>
2020-10-04 15:02:49 -0700

    store catalog showing up with stub data

--- web/src/components/Catalog.js
+++ web/src/components/Catalog.js
@@ -3,6 +3,14 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchProducts } from '../actions'
+import {
+  Card,
+  CardImg,
+  CardBody,
+  Col,
+  Container,
+  Row,
+} from 'reactstrap';
 class Catalog extends Component {
   constructor(props) {
     super(props);
@@ -21,8 +29,32 @@ class Catalog extends Component {
       console.log('catalog props.products ',this.props.products)
 
       return (
-        <div><p>This is the catalog</p></div>
-        );
+        <div className="album py-5 bg-light">
+            <Container>
+                <Row>
+                    {this.props.products.list.map((item, key) => {
+                        return (
+                            <Col md="4" key={key}>
+                                <Card className="mb-4 box-shadow">
+                                <Row><Col><p class="ml-3 mt-3 pt-1">Category: {item.category}</p></Col>
+                                <Col><h2 class="text-right mr-3 pt-1">${item.price}</h2></Col></Row>
+                                <CardBody style={{marginTop: '-35px'}}>
+                                    <CardImg
+                                        top
+                                        width="100%"
+                                        src={item.image}
+                                        alt={item.name}
+                                    />
+                                    </CardBody>
+                                    <h3 class="text-center" style={{marginTop: '-20px'}}>{item.name}</h3>
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </div>
+    );
 };
 };
 function mapStateToProps(state) {

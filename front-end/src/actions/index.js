c68b2f0d404d9a3bbe8d3b105cb779f5f07c1f39
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-03 21:31:05 -0400

    adds a two base fetch actions for the initial load of the page as well as fetch products by category

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -1,1 +1,25 @@
 import axios from 'axios';
+
+export const FETCH_PRODUCTS = 'fetch_products';
+export const FETCH_PRODUCTS_BY_CATEGORY = 'fetch_products_by_category';
+
+const ROOT_URL = 'localhost:8000/products';
+
+// initial fetch posts, should load the first 9 products on page load
+export function fetchProducts() {
+    const request = axios.get(`${ROOT_URL}`)
+
+    return {
+        type: FETCH_PRODUCTS,
+        payload: request
+    }
+}
+// fetch posts based on a category parameter
+export function fetchProductsByCategory(category) {
+    const request = axios.get(`${ROOT_URL}?category=${category}`);
+
+    return {
+        type: FETCH_PRODUCTS_BY_CATEGORY,
+        payload: request
+    }
+}

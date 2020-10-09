c1a7d7c1ee8cf2e98705cefa5d258efbe855b0d4
Aissa Feldmann <aissaf@umich.edu>
2020-10-09 13:35:46 -0400

    all categories added to dropdown

--- front-end-react/src/containers/category-dropdown.js
+++ front-end-react/src/containers/category-dropdown.js
@@ -15,30 +15,45 @@ class CategoryDropdown extends Component {
   change(event) {
     //setState is async. Use second parameter function that runs after state is set
     this.setState({ category: event.target.value }, function () {
-      console.log('in cat container & this.state.category: ' + this.state.category);
+      console.log(
+        'in cat container & this.state.category: ' + this.state.category
+      );
       this.props.fetchProducts('', this.state.category, '', '');
     });
   }
 
   render() {
     return (
-          <span>
-            <select
-              id='categories'
-              className='select-category'
-              placeholder='Select category'
-              value={this.state.category}
-              onChange={this.change}>
-              <option defaultValue>Select a category</option>
-              <option value='Tools'>Tools</option>
-              <option value='Garden'>Garden</option>
-              <option value='Shoes'>Shoes</option>
-              <option value='Books'>Books</option>
-              <option value='Clothing'>Clothing</option>
-              <option value='Health'>Health</option>
-              <option value='Jewelery'>Jewelery</option>
-            </select>
-          </span>
+      <span>
+        <select
+          id='categories'
+          className='select-category'
+          placeholder='Select category'
+          value={this.state.category}
+          onChange={this.change}>
+          <option defaultValue>Select a category</option>
+          <option value='Baby'>Baby</option>
+          <option value='Beauty'>Beauty</option>
+          <option value='Books'>Books</option>
+          <option value='Clothing'>Clothing</option>
+          <option value='Computers'>Computers</option>
+          <option value='Electronics'>Electronics</option>
+          <option value='Games'>Games</option>
+          <option value='Garden'>Garden</option>
+          <option value='Grocery'>Grocery</option>
+          <option value='Health'>Health</option>
+          <option value='Home'>Home</option>
+          <option value='Industrial'>Industrial</option>
+          <option value='Jewelery'>Jewelery</option>
+          <option value='Kids'>Kids</option>
+          <option value='Music'>Music</option>
+          <option value='Outdoors'>Outdoors</option>
+          <option value='Shoes'>Shoes</option>
+          <option value='Sports'>Sports</option>
+          <option value='Tools'>Tools</option>
+          <option value='Toys'>Toys</option>
+        </select>
+      </span>
     );
   }
 }
--- front-end-react/src/containers/product-list.js
+++ front-end-react/src/containers/product-list.js
@@ -20,7 +20,6 @@ class ProductList extends Component {
   }
 
   handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
     this.setState({ page: pageNumber }, function () {
       this.props.fetchProducts('', '', '', this.state.page);
       console.log("this.state.page: " + this.state.page);
@@ -54,8 +53,6 @@ class ProductList extends Component {
           itemsCountPerPage={9}
           totalItemsCount={this.props.products.count}
           pageRangeDisplayed={5}
-          // itemClass="page-item"
-          // linkClass="page-link"
           onChange={this.handlePageChange.bind(this)}
           />
           <div className='product-count'>Total products: {this.props.products.count}</div>

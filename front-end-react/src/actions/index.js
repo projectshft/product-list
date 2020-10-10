b79c73d75c099f807e20af960322c50f55fc74ec
Aissa Feldmann <aissafeldmann@Aissas-Air.attlocal.net>
2020-10-10 18:53:08 -0400

    first mac commit! editing actions

--- front-end-react/src/actions/index.js
+++ front-end-react/src/actions/index.js
@@ -6,43 +6,43 @@ const ROOT_URL = 'http://localhost:8000/products';
 let request = '';
 
 export function fetchProducts(search, category, sort, page) {
-  if (search && !category && !sort && !page) {
-    request = axios.get(`${ROOT_URL}?search=${search}`); //a promise
-    console.log('FetchProducts Search', request);
-    return {
-      type: FETCH_PRODUCTS, //FETCH_PRODUCTS is action name"
-      payload: request, //api call
-    };
-
-  } else if (!search && category && !sort && !page) {
-    request = axios.get(`${ROOT_URL}?category=${category}`); //a promise
-    console.log('FetchProducts Category', request);
-    return {
-      type: FETCH_PRODUCTS, //action name"
-      payload: request, //api call
-    };
-
-  } else if (!search && !category && sort && !page) {
-    request = axios.get(`${ROOT_URL}?sort=${sort}`); //a promise
-    console.log('FetchProducts Sort ', request);
-    return {
-      type: FETCH_PRODUCTS, //action name"
-      payload: request, //api call
-    };
-
-  } else if (!search && !category && !sort && page) {
-    request = axios.get(`${ROOT_URL}?page=${page}`); //a promise
-    console.log('FetchProducts Page ', request);
-    return {
-      type: FETCH_PRODUCTS, //action name"
-      payload: request, //api call
-    };
-  }
-
-  request = axios.get(`${ROOT_URL}`); //a promise
+  request = axios.get(`${ROOT_URL}?search=${search}`); //a promise
   console.log('FetchProducts Request', request);
   return {
     type: FETCH_PRODUCTS, //FETCH_PRODUCTS is action name"
     payload: request, //api call
   };
 }
+
+// if (search && !category && !sort && !page) {
+//   request = axios.get(`${ROOT_URL}?search=${search}`); //a promise
+//   console.log('FetchProducts Search', request);
+//   return {
+//     type: FETCH_PRODUCTS, //FETCH_PRODUCTS is action name"
+//     payload: request, //api call
+//   };
+
+// } else if (!search && category && !sort && !page) {
+//   request = axios.get(`${ROOT_URL}?category=${category}`); //a promise
+//   console.log('FetchProducts Category', request);
+//   return {
+//     type: FETCH_PRODUCTS, //action name"
+//     payload: request, //api call
+//   };
+
+// } else if (!search && !category && sort && !page) {
+//   request = axios.get(`${ROOT_URL}?sort=${sort}`); //a promise
+//   console.log('FetchProducts Sort ', request);
+//   return {
+//     type: FETCH_PRODUCTS, //action name"
+//     payload: request, //api call
+//   };
+
+// } else if (!search && !category && !sort && page) {
+//   request = axios.get(`${ROOT_URL}?page=${page}`); //a promise
+//   console.log('FetchProducts Page ', request);
+//   return {
+//     type: FETCH_PRODUCTS, //action name"
+//     payload: request, //api call
+//   };
+// }

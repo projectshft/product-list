53e901d0c28482e3446aae9662d795e02075e65d
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-12 14:00:42 -0400

    adds route for product return with pagination

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,24 @@ const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
 
+//get products w/ server pagination
+router.get('/products', (req, res) => {
+  const page = req.query.page || 1
+  const perPage = 9
+  const skipNum = ((page * perPage) - perPage);
+  
+  Product.find({}).skip(skipNum).limit(9).exec(function (err, products) {
+    if (err) return console.log(err);
+    res.send(products)
+  });
+
+});
+
+module.exports = router;
+
+
+/*****************************Testing*****************************
+
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
@@ -18,10 +36,5 @@ router.get("/generate-fake-data", (req, res, next) => {
   res.end();
 });
 
-router.get("/products", (req, res, next) => {
-  Product.find((error, products) => {
-    res.send(products);
-  });
-});
 
-module.exports = router;
+*/

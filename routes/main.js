612691b8d16fc92b6d61c0def63eec7be4c844d2
unknown <joshkamrass@gmail.com>
2021-05-13 09:31:55 -0400

    adds endpoint for retrieving single product

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
+const products = require("./products");
 const Review = require("../models/review");
 const mongoose = require("mongoose");
+
+router.use("/products", products);
 
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
@@ -19,20 +22,6 @@ router.get("/generate-fake-data", (req, res, next) => {
     });
   }
   res.end();
-});
-
-router.get("/products", (req, res, next) => {
-  const page = req.query.page || 1;
-  const perPage = 9;
-  Product.find({})
-    .skip((page-1)*perPage)
-    .limit(perPage)
-    .exec((err, products) => {
-      if(err) {
-        console.log(err);
-      }
-      res.send(products);
-    })
 });
 
 module.exports = router;
---
+++ routes/products.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const Product = require("../models/product");
+const Review = require("../models/review");
+const mongoose = require("mongoose");
+
+router.param("product", (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if(err) {
+      console.error(err)
+    }
+    req.product = product;
+    next();
+  })
+})
+
+router.get("/", (req, res, next) => {
+  const page = req.query.page || 1;
+  const perPage = 9;
+  Product.find({})
+    .skip((page-1)*perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      if(err) {
+        console.log(err);
+      }
+      res.send(products);
+    })
+});
+
+router.get("/:product", (req, res, next) => {
+  if(!req.product) {
+    res.status(404).send("Product Not Found");
+  }
+  res.send(req.product)
+})
+
+router.get("/:product/reviews", (req, res, next) => {
+
+})
+
+module.exports = router;

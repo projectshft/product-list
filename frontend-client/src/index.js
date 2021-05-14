542320777b39064d4a7e2ff9814a2dac201f4af8
unknown <joshkamrass@gmail.com>
2021-05-14 08:44:08 -0400

    redux and basic component init

--- frontend-client/src/index.js
+++ frontend-client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(rootReducer, applyMiddleware(promise))}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend-client/src/reducers/products-reducer.js
@@ -0,0 +1,5 @@
+const productsReducer = (state = null, action) => {
+  return state;
+}
+
+export default productsReducer;

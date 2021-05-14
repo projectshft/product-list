73a817e1dd9d36f4881c7a65e483147f97a1343f
unknown <joshkamrass@gmail.com>
2021-05-14 10:07:52 -0400

    adds pagination display

--- frontend-client/src/components/PageSelector.js
+++ frontend-client/src/components/PageSelector.js
@@ -1,7 +1,21 @@
 const PageSelector = () => {
   return (
-    <div>
-      <h1>Page Selector Goes Here</h1>
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }

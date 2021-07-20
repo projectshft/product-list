5c3ff21c0d97790253e6399a24653f0d7f28c3b7
Jeremy Wheless <whelessjw@gmail.com>
2021-07-20 10:56:58 -0400

    added middleware to save product to req.product

--- models/product.js
+++ models/product.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const ReviewSchema = new Schema({
+  userName: String,
+  text: String,
+  product: { type: Schema.Types.ObjectId, ref: "Product" },
+});
+
+const Review = mongoose.model("Review", ReviewSchema);
+
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
   image: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
-module.exports = mongoose.model("Product", ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
 
+router.param("product", async function (req, res, next, id) {
+  req.product = await Product.findById(id);
+
+  if (!req.product) {
+    throw new Error("No product found with id provided");
+  }
+
+  next();
+});
+
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
@@ -10,12 +20,36 @@ router.get("/generate-fake-data", (req, res, next) => {
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
+    product.reviews = [];
 
     product.save((err) => {
       if (err) throw err;
     });
   }
   res.end();
+});
+
+router.get("/products", (req, res, next) => {
+  const perPage = 9;
+
+  // return the first page by default
+  const page = req.query.page || 1;
+
+  Product.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+      });
+    });
+});
+
+router.get("/products/:product", (req, res, next) => {
+  res.json(req.product);
 });
 
 module.exports = router;

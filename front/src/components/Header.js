5de52228d913c47003b42fdaa785ae0c642930e5
Jeremy Wheless <whelessjw@gmail.com>
2021-07-21 15:28:58 -0400

    added function to get all categories from products

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const categories = useSelector((state) =>
+    state.products.reduce((acc, product) => {
+      if (!acc.includes(product.category)) {
+        return [...acc, product.category];
+      } else {
+        return acc;
+      }
+    }, [])
+  );
+
+  console.log(categories);
   return (
     <div className="container header">
       <div className="row">

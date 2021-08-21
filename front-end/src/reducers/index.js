365e0d698c1ecb5e86be20df2b89d86e657452d5
AlsoDanPiston <djpiston@gmail.com>
2021-08-21 16:31:01 -0400

    moved front-end back out to work on it

--- front-end/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import ProductReducer from "./reducer-product";
-
-// added to make expanding this app easier, not exactly necessary for this simple app
-const rootReducer = combineReducers({
-  products: ProductReducer,
-});
-
-export default rootReducer;

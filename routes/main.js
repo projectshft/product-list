14608bf574c86f4619e37d687e013734d9019615
Otto Frankel <ottofrankel@comcast.net>
2021-09-10 14:22:25 -0700

    Created react app

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
+const Review = require("../models/review");
 
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
@@ -19,7 +20,8 @@ router.get("/generate-fake-data", (req, res, next) => {
 });
 
 router.param("product", (req, res, next, id) => {
-  Product.findOne({_id: id})
+  Product
+  .find({_id: id})
   .populate("reviews")
   .exec((err, product) => {
     if (err)
@@ -31,7 +33,6 @@ router.param("product", (req, res, next, id) => {
 })
 
 router.get("/products", (req, res, next) => {
-
   const pageNum = req.query.page || 1;
 
   Product
@@ -47,8 +48,42 @@ router.get("/products", (req, res, next) => {
   });
 });
 
-router.get("/products/:product", (req, res, next) => {
+router.get("/products/:product", (req, res) => {
   res.send(req.product);
+})
+
+router.get("/products/:product/reviews", (req, res, next) => {
+  const pageNum = req.query.page || 1;
+
+  Review
+  .find({product: req.product._id})
+  .skip(4 * (pageNum - 1))
+  .limit(4)
+  .exec((err, reviews) => {
+    Review.count().exec((err, count) => {
+      if (err) return next(err);
+
+      res.send(reviews);
+    });
+  })
+})
+
+router.post("/products/:product/reviews", (req, res) => {
+  const product = req.product;
+
+  // This is for testing 
+  const testReview = new Review({
+    text: 'test review',
+    username: 'me',
+    product: product._id
+  })
+
+  testReview.save();
+
+  product.reviews.push(testReview);
+  product.save();
+
+  res.send(testReview);
 })
 
 module.exports = router;

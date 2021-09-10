02f94eb4a4b8b854cc5026206d36843e6159be32
zdresser <42179244+zdresser@users.noreply.github.com>
2021-09-10 13:17:50 -0400

    Added 3 routes

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,19 @@
+const express = require("express");
 const router = require("express").Router();
+const app = express();
 const faker = require("faker");
+
 const Product = require("../models/product");
 const Review = require("../models/review");
+app.use(express.json());
+//add function to build out categories to call in queries that ///use categories
 
-//build out categories for use in later queries? Middleware? 
+//create multiple router files? 
+
+//function to find product and avoid repetition?
+const findProduct = (req) => {
+
+}
 
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
@@ -41,11 +51,50 @@ router.get("/products/:product", (req, res, next) => {
   Product.findById(req.params.product)
     .exec((err, product) => {
       if (product) {
+        
        res.send(product) 
-      } else {
-        res.sendStatus(404)
+      } else if (err) {
+        next(err)
       }
     })
 })
+
+router.get("/products/:product/reviews", (req, res, next) => {
+  Product.findById(req.params.product)
+    .populate("reviews")
+    .exec((err, product) => {
+      if (product) {
+       res.send(product.reviews) 
+      } else if (err) {
+        next(err)
+      }
+    });    
+});
+
+router.post("/products", (req, res, next) => {
+  let newProduct = new Product(req.body);
+
+  newProduct.save((err, product) => {
+    if (err) {
+      next(err)
+    } 
+    res.send(product)
+  });    
+});
+
+router.post("/products/:product/reviews", (req, res, next) => {
+  let newReview = new Review({
+    userName: req.body.userName,
+    text: req.body.text,
+    product: req.params.product
+  });
+
+  newReview.save((err, review) => {
+    if (err){
+      next(err)
+    }
+    res.send(review)
+  });  
+});
 
 module.exports = router;

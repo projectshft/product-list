f6109660fe74e3761ff4759a587e8eb621c5878d
Benjamin Pryor <bscott23@gmail.com>
2021-09-11 16:30:18 -0400

    added search functions

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 
-export function fetchProducts() {
-  const request = axios.get('http://localhost:8000/products')
+export function fetchProducts(search, price) {
+  const request = axios.get('http://localhost:8000/products', {
+    params: {
+      query: search,
+      price: price
+    }
+  })
 
   return {
     type: FETCH_PRODUCTS,
--- front-end/src/components/search-bar.js
+++ front-end/src/components/search-bar.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux"
+import { fetchProducts } from "../actions";
+
 function SearchBar() {
-return (
+const dispatch = useDispatch();
+const [search, setSearch] = useState();
+const [price, setPrice] = useState();
+
+const handleSearchChange = (e) => {
+  setSearch(e.target.value)
+  if (e.keyCode === 13) {
+    handleSearch()
+  }
+}
+
+const handleSearch = () => {
+  dispatch(fetchProducts(search, price))
+}
+
+const handleCategoryChange = () => {
+
+}
+
+const handlePriceSort = (e) => {
+  setPrice(e.target.value)
+  handleSearch()
+}
+  return (
   <div className='row'>
     <div className='col-md-6 offset-md-1'>
       <div className='input-group'>
-        <input type='text' className='form-control' placeholder='Product Search'></input>
+        <input type='text' className='form-control' placeholder='Product Search' onKeyUp={handleSearchChange}></input>
       </div>
     </div>
     <div className='col-md-2'> 
-      <select className="form-control" name="category" >
+      <select className="form-control" name="category" onChange={handleCategoryChange} >
         <option>Sort by Category</option>
         <option value="Automotive">Automotive</option>
         <option value="Clothing">Clothing</option>
@@ -18,10 +45,10 @@ return (
       </select>
     </div>
     <div className='col-md-2'> 
-      <select className="form-control" name="category" >
+      <select className="form-control" name="price" onChange={handlePriceSort} >
         <option>Sort by Price</option>
-        <option value="Highest">Highest</option>
-        <option value="Lowest">Lowest</option>         
+        <option value="highest">Highest</option>
+        <option value="lowest">Lowest</option>         
       </select>
     </div>
   </div>

7c21553e2dd1ca007e823d5400e24f5889ed276f
Ryan Passer <rpasser93@gmail.com>
2021-09-11 06:30:15 -0400

    Add GET products-product route

--- routes/main.js
+++ routes/main.js
@@ -2,21 +2,34 @@ const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
 
-router.get("/generate-fake-data", (req, res, next) => {
-  for (let i = 0; i < 90; i++) {
-    let product = new Product();
+router.get("/products", (req, res, next) => {
+  const perPage = 9;
 
-    product.category = faker.commerce.department();
-    product.name = faker.commerce.productName();
-    product.price = faker.commerce.price();
-    product.image = "https://via.placeholder.com/250?text=Product+Image";
-    product.reviews = [];
+  // return the first page by default
+  const page = req.query.page || 1;
 
-    product.save((err) => {
-      if (err) throw err;
+  Product.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+      });
     });
-  }
-  res.end();
+});
+
+router.get("/products/:product", (req, res, next) => {
+  const {product} = req.params; 
+
+  Product.find({_id: product})
+    .exec((err, product) => {
+      if (err) return next(err);
+
+      res.send(product);
+    });
 });
 
 module.exports = router;

d23dfe6aa55dd985df80a5266fa4df9e543006c5
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-09-13 12:06:14 -0400

    Began setup of project

---
+++ routes/main.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const faker = require("faker");
+const Product = require("../models/product");
+
+router.get("/generate-fake-data", (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product();
+
+    product.category = faker.commerce.department();
+    product.name = faker.commerce.productName();
+    product.price = faker.commerce.price();
+    product.image = "https://via.placeholder.com/250?text=Product+Image";
+
+    product.save((err) => {
+      if (err) throw err;
+    });
+  }
+  res.end();
+});
+
+router.get("/products", (req, res, next) => {
+  const perPage = 9;
+
+  // return the first page by default
+  const page = req.query.page || 1;
+
+  Product.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+      });
+    });
+});
+
+module.exports = router;

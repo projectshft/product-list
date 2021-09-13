85b5432a846e908a31f01797911bc74af12a89da
Ryan Passer <rpasser93@gmail.com>
2021-09-12 20:15:49 -0400

    Tie frontend file into backend project

---
+++ frontend/src/components/App.js
@@ -0,0 +1,20 @@
+
+import ProductList from './product-list';
+
+const App = () => {
+
+  return (
+  <div className="container">
+    <div className="row">
+      <h1>Testing!</h1>
+    </div>
+
+
+    <div className="row">
+      <ProductList />
+    </div>
+  </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/product-list.js
@@ -0,0 +1,12 @@
+
+const ProductList = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Table Here</h2>
+      </div>
+    </div>
+  )
+}
+
+export default ProductList;

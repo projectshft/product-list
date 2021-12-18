f2d4bd66a4adf0c9b1df9789a5478e99e7fee3c4
Jason Stover <jstover8807@gmail.com>
2021-12-18 15:39:47 -0500

    Second commit figuring out elemMatch

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const faker = require("faker");
-const product = require("../models/product");
+
 const Product = require("../models/product");
 const Review = require("../models/review");
 
@@ -36,26 +36,44 @@ router.get("/generate-fake-data", (req, res, next) => {
 
 router.get("/products", (req, res, next) => {
     const perPage = 9;
-
-    // return the first page by default
     const page = req.query.page || 1;
     searchedCategory = req.query.category;
+    searchedQuery = req.query.query;
+    searchedSorting = req.query.price;
 
+    
 
+    if (searchedCategory && !searchedQuery && !searchedSorting) {
+        Product.find({category: req.query.category})
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec((err, searchedProducts) => {     
+            if (err) return next(err);
+            res.send(searchedProducts);         
+        });            
+    } else if (!searchedCategory && searchedQuery && !searchedSorting) {
+        Product.find().elemMatch('product', { category: req.query.query, name: req.query.query })  //Can't get this to work
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec((err, searchedProducts) => {     
+                if (err) return next(err);
+                res.send(searchedProducts);         
+            });            
+    }
+    
+    
+    else  {
     Product.find({})
         .skip(perPage * page - perPage)
         .limit(perPage)
-        .exec((err, products) => {
-        // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
-        
+        .exec((err, products) => {     
         
         Product.count().exec((err, count) => {
             if (err) return next(err);
-
-            res.send(products);
-          
+            res.send(products);          
         });
         });
+    }
 });
 
 // working but need to add pagination

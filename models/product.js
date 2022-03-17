203cad945d4b2e1de466ea4b1e2ab3a90ac5bdeb
aelizabethian <amanda.richardson2@gmail.com>
2022-03-17 17:08:41 -0400

    Initial setup and the routes

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const review = require('./review')
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  category: String,
+  name: String,
+  price: Number,
+  image: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "review" }],
+});
+
+
+
+
+module.exports = mongoose.model("Product", ProductSchema);

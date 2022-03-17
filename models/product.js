9848210dc41ff3e2124b022b24bce550db981b2a
Nathan Drake <nathandrakedrums@gmail.com>
2022-03-17 11:18:41 -0500

    init

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema({
+  category: String,
+  name: String,
+  price: Number,
+  image: String,
+});
+
+module.exports = mongoose.model('Product', ProductSchema);

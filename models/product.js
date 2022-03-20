59c964d0f7b500470f57221fc2949115ed8bdc12
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-03-20 13:18:35 -0500

    reviewSchema / reviews array added to product

--- models/product.js
+++ models/product.js
@@ -4,12 +4,26 @@ const mongoose = require("mongoose");
 // shorter variable for mongoose schema
 const Schema = mongoose.Schema;
 
+// create a review Schema
+const reviewSchema = new Schema({
+  userName: String,
+  text: String,
+  product: { type: Schema.Types.ObjectId, ref: "Product"},
+});
+
+const Review = mongoose.model("review", reviewSchema);
+
 // create a Product Schema
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
   image: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId, ref: "review"
+    }
+  ],
 });
 
 // export Product model using ProductSchema

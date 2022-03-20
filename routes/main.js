915f329c43f3b1674395d15a4cb611b6dacf2a6a
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-03-20 12:45:59 -0500

    faker versioned back. database loaded

--- routes/main.js
+++ routes/main.js
@@ -1,28 +1,21 @@
-// require in express router, faker, and product model
-const router = require("express");
+const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
 
-// setup a route to get dummy data
-router.length("/generate-fake-data", (req, res, next) => {
+router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
-    // looping through dummy data and creating news instances of Product Model from it.
     let product = new Product();
 
-    // assigining properties via the ProductSchema
-    product.category = faker.commerce.deparment();
+    product.category = faker.commerce.department();
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
 
-    // save each newly created Product to the database
     product.save((err) => {
       if (err) throw err;
     });
   }
-  // end response
   res.end();
-}); 
+});
 
-// export the router
 module.exports = router;

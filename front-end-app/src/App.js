22a66883dd340a2174d1942129ec2fde74f7474a
Michael Grumbine <mgrumbine3@gmail.com>
2022-03-21 13:45:43 -0700

    React App up and running, and first component skeleton finished

---
+++ front-end-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Filters from './components/Filters.js';
+import Products from './components/Products.js';
+import Pages from './components/Pages.js';
+
+function App() {
+  return (
+    <Filters />
+  );
+}
+
+export default App;
---
+++ front-end-app/src/components/Filters.js
@@ -0,0 +1,36 @@
+import {Container, Row, Col, Form} from 'react-bootstrap';
+
+const Filters = () => {
+  return (
+    <Container className="filter-bar">
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3">
+            <Form.Control placeholder='Search By Product Name' onChange={(test) => {console.log(test)}}/>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Select onChange={(e) => {console.log(e)}}>
+              <option>Filter By Category</option>
+              <option>Tools</option>
+              <option>Garden</option>
+              <option>Health</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Select onChange={(e) => {console.log(e)}}>
+              <option>Filter By Price</option>
+              <option>Lowest to Highest</option>
+              <option>Highest to Lowest</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Container>
+  )
+};
+
+export default Filters;

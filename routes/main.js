fa3e68a111dec1ebb9991370fb274745d6850249
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-04-21 17:11:07 -0400

    I can sort by price in an ascending order

--- routes/main.js
+++ routes/main.js
@@ -21,12 +21,15 @@ const {Product, Review} = require("../models/product");
 router.get("/products", async (req, res, next) => {
   const perPage = 9;
 
+
+
   // return the first page by default
   const page = req.query.page || 1;
 
   await Product.find(req.query)
     .skip(perPage * page - perPage)
     .limit(perPage)
+    .sort({price: 1})
     .exec((err, products) => {
       // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
       Product.count().exec((err, count) => {
@@ -35,6 +38,7 @@ router.get("/products", async (req, res, next) => {
         res.send(products);
       });
     });
+
 });
 
 router.get("/reviews", async (req, res, next) => {

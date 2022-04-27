8b4bedd83c8adb6c35362a2ee5561c8b2e0d895e
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-04-27 08:04:40 -0400

    tested the connection between front end and back end.

--- product-list-front-end/src/App.js
+++ product-list-front-end/src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import ContentDisplay from "./components/ContentDisplay";
 
 
+
 function App() {
+  useEffect(() => {
+    const request = async () =>{
+      await fetch('http://localhost:8000/products', {
+        method: 'GET',
+        mode: 'cors'
+      })
+    }
+    request();
+  }, []);
+
   return (
     <div>
       <SearchBar />

14ce07ffa3f1e04b8cec3ac5a7870ce8580be88f
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-07 12:50:50 -0400

    able to successfully console.log the data from the fetch

--- product-list-front-end/src/App.js
+++ product-list-front-end/src/App.js
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "./Components/SearchBar";
 import ContentDisplay from "./Components/ContentDisplay";
 
 
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const request = async () =>{
       await fetch('http://localhost:8000/products', {
         method: 'GET',
         mode: 'cors'
       })
+      .then((res) => {
+        // setProducts(res.data)
+        console.log(res);
+        res.json().then((data) => {
+          console.log(data)
+        })
+      })
     }
     request();
   }, []);
+
 
   return (
     <div>

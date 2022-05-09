f63a93b63d9fabb94300dfc69bcb048fef825f6d
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-05-09 17:23:59 -0400

    the css is acceptable to work with

--- product-list-front-end/src/components/ContentDisplay.js
+++ product-list-front-end/src/components/ContentDisplay.js
@@ -9,10 +9,10 @@ const ContentDisplay = (props) => {
             {products.map((data, index) => (
               <div className="col-sm-4" key={index}>
                   <div id="product">
-                  <div id="category" >category: {data.category}</div>
-                  <div id="price">price: {data.price}</div>
+                  <div id="category" >Category: {data.category}</div>
+                  <div id="price">Price: {data.price}</div>
                   <img id="img" src={data.image} alt="new"/>
-                  <div id="name">name: {data.name}</div>
+                  <div id="name">Name: {data.name}</div>
               </div>
               </div>
             ))}

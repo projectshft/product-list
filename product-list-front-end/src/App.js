c8353fbf8817c577498065a2ba75da54c2d2e1fc
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-14 15:31:55 -0400

    refactored everything back to pagination component

--- product-list-front-end/src/App.js
+++ product-list-front-end/src/App.js
@@ -65,7 +65,7 @@ function App() {
       <div>
         <SearchBar />
         {dispatched && <ContentDisplay />}
-        {/* <Pagination /> */}
+        <Pagination />
       </div>
     </div>
   );
--- product-list-front-end/src/components/ContentDisplay.js
+++ product-list-front-end/src/components/ContentDisplay.js
@@ -2,13 +2,6 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const ContentDisplay = () => {
-  const [pageNumber, setPageNumber] = useState(1);
-  
-  const urlBase = 'http://localhost:8000/products';
-
-  const previous = '<<previous'
-  const next = 'next>>'
-
   const productData = useSelector(state => state[0]);
 
 
@@ -31,9 +24,6 @@ const ContentDisplay = () => {
         
         </div>
       </div>
-      <div className="right-align">
-          <p>{previous}</p> <p>1</p> <p>2</p> <p>3</p> <p>4</p> <p>5</p> <p>6</p> <p>7</p> <p>8</p> <p>9</p> <p>10</p><p>11</p><p>{next}</p>
-        </div>
     </div>
   )
   

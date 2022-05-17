b72ee711fcf35e133348c82461d95cff524837fa
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-17 10:39:53 -0400

    all the category selectors work except for All

--- product-list-front-end/src/components/SearchBar.js
+++ product-list-front-end/src/components/SearchBar.js
@@ -1,7 +1,18 @@
+import { fetchProducts } from "../Redux/actions";
+import { useDispatch } from "react-redux";
+
 const SearchBar = () => {
+  const dispatch = useDispatch();
   
-  const searchByCategory = () => {
-    console.log('test');
+  const searchByCategory = async (e) => {
+    const value = e.target.value;
+    const url = `http://localhost:8000/products?category=${value}`
+    await fetch(url, {
+      method: 'GET', 
+      mode: 'cors'
+    })
+    .then((res) => res.json())
+    .then((data) => dispatch(fetchProducts(data)));
   }
 
   const categories = ['Baby', 'Beauty', 'Books', 'Clothing', 'Computers', 'Electronics', 'Games', 'Grocery', 'Home', 'Jewelry', 'Kids', 'Movies', 'Music', 'Outdoors', 'Shoes', 'Sports', 'Tools', 'Toys'];
@@ -10,7 +21,7 @@ const SearchBar = () => {
   <div>
     <input type="text" placeholder="Search"/>
     <label htmlFor="categories">Choose a Category: </label>
-    <select id="categories">
+    <select id="categories" onChange={searchByCategory}>
       <option value='' key="0">All</option>
       {categories.map((cat, index) => {
         return <option value={`${cat}`} key={index + 1}>{cat}</option>

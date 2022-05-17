02e1aef5c566749c61207f0ae8d63f591e2bcfa8
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-17 09:41:29 -0400

    search bar maps through categories

--- product-list-front-end/src/components/SearchBar.js
+++ product-list-front-end/src/components/SearchBar.js
@@ -11,27 +11,8 @@ const SearchBar = () => {
     <input type="text" placeholder="Search"/>
     <select>
       {categories.map((cat, index) => {
-        <option value={`${cat}`} key={index}>cat</option>
+        return <option value={`${cat}`} key={index}>{cat}</option>
       })}
-
-    {/* <option value="Baby" onClick={() => searchByCategory()}>Baby</option>
-    <option value="Beauty">Beauty</option>
-    <option value="Books">Books</option>
-    <option value="Clothing">Clothing</option>
-    <option value="Computers">Computers</option>
-    <option value="Electronics">Electronics</option>
-    <option value="Games">Games</option>
-    <option value="Grocery">Grocery</option>
-    <option value="Home">Home</option>
-    <option value="Jewelry">Jewelry</option>
-    <option value="Kids">Kids</option>
-    <option value="Movies">Movies</option>
-    <option value="Music">Music</option>
-    <option value="Outdoors">Outdoors</option>
-    <option value="Shoes">Shoes</option>
-    <option value="Sports">Sports</option>
-    <option value="Tools">Tools</option>
-    <option value="Toys">Toys</option> */}
     </select>
   </div>
   )

549abfba10f71b22466db10d73e2d759fdfc51ff
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-06-03 19:42:13 -0500

    created GET path for product by id

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,32 @@
 const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
+const Review = require("../models/review");
 const queryString = require('querystring');
 
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
+    let review = new Review();
+
+    review.username = faker.internet.userName();
+    review.text = faker.commerce.productName();
+    review.product = product._id;
 
     product.category = faker.commerce.department();
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
-
+    product.reviews = [review._id];
+    
     product.save((err) => {
       if (err) throw err;
     });
+
+    review.save((err) => {
+      if (err) throw err;
+    });
+
   }
   res.end();
 });
@@ -35,5 +47,40 @@ router.get("/products", (req, res, next) => {
       });
     });
 });
+
+/* ===================
+  GET products by id
+  =================== */
+router.get("/products/:product", (req, res, next) => {
+  const { product } = req.params;
+
+  Product.findOne({ _id: product })
+    .populate("reviews")
+    .exec((err, returnProduct) => {
+      if (err) return next(err);
+
+      res.send(returnProduct);
+    });
+})
+
+/* =====================================
+  GET all reviews for a product (limit 4)
+  ===================================== */
+
+/* ===================
+  POST new product
+  =================== */
+
+/* ===========================
+  POST new review to a product
+  ========================== */
+
+/* ===================
+  DELETE product by id
+  =================== */
+
+/* ===================
+  DELETE review by id
+  =================== */
 
 module.exports = router;

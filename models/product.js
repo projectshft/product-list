6ce8d1efc73cbbe8345b7473b2e2ffd777c2d909
Emily Freeman <freeman.ecf@gmail.com>
2022-06-17 11:40:58 -0400

    api working

--- models/product.js
+++ models/product.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const ReviewSchema = new Schema({
+  username: String,
+  text: String
+})
+
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
   image: String,
+  reviews: [ReviewSchema]
 });
 
-module.exports = mongoose.model("Product", ProductSchema);
+
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
 
+
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
@@ -24,12 +25,82 @@ router.get("/products", (req, res, next) => {
   
     Product.find().skip(amountToSkip).limit(9).exec((err, products) => {
       if (err) {
-        console.log(err)
+        next(err)
       }
       else {
         res.send(products)
       }
-    })
+    });
 });
+
+router.get("/products/:product", (req, res) => {
+  const productId = req.params.product;
+  Product.findById(productId).exec((err, product) => {
+    if (err) res.status(400).send(err.message);
+
+    res.send(product);
+  });
+});
+
+router.get("/products/:product/reviews", (req, res) => {
+  const productId = req.params.product;
+  Product.findById(productId).exec((err, product) => {
+    if (err) res.status(400).send(err.message);
+    res.send(product.reviews);
+  });
+});
+
+//ADD PAGINATION
+
+router.post("/products", (req, res) => {
+  if (req.body.category && req.body.name && req.body.price && req.body.image) {
+    Product.create({
+      category: req.body.category,
+      name: req.body.name,
+      price: req.body.price,
+      image: req.body.image,
+      reviews: req.body.reviews
+    })
+  } else {
+    res.status(400).send('all fields must be filled out')
+  }
+});
+
+router.post("/products/:product/reviews", (req, res) => {
+  const productId = req.params.product;
+
+  if (req.body.username && req.body.text) {
+    Product.findById(productId).exec((err, product) => {
+      const newReview = {
+        username: req.body.username,
+        text: req.body.text
+      };
+      product.reviews.push(newReview);
+      product.save();
+    });
+   
+  }
+  else {
+    res.status(400).send('all fields must be filled out')
+  }
+});
+
+router.delete("/products/:product", (req, res) => {
+  const productId = req.params.product;
+
+  Product.deleteOne({_id: productId}).exec((err, product) => {
+    res.send("product has been removed");
+  });
+})
+
+router.delete("/reviews/:review", (req, res) => {
+  const reviewId = req.params.review;
+
+  Product.find({'reviews._id': reviewId}).exec((err, product) => {
+    product[0].reviews.pull(reviewId);
+    res.send(product);
+  });
+});
+
 
 module.exports = router;

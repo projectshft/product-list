e1545a0a47fa0c0e4c9385a4e445634ebc2e902a
Alex Rodgers <skyewulff@outlook.com>
2022-06-23 22:07:45 -0400

    create Control component that will house the search bar and category/sort dropdowns

---
+++ product-list-app/src/components/Controls.js
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+import './Controls.css';
+
+const Controls = () => {
+    return (
+        <div className="ProductList">
+            <h1>Controls</h1>
+            
+        </div>
+    )
+};
+
+export default Controls;

2a9b4b2f32cd5c75e0348c4dc471b00d62d1eb96
Alex Rodgers <skyewulff@outlook.com>
2022-06-24 23:17:10 -0400

    fix but in product reducer that assigned the entire payload object to the state instead of the data array in the payload object

--- product-list-app/src/reducers/index.js
+++ product-list-app/src/reducers/index.js
@@ -5,16 +5,8 @@ const DEFAULT_STATE = [];
 const productsReducer = (state = DEFAULT_STATE, action) => {
     switch (action.type) {
         case FETCH_PRODUCTS:
-            let foundProducts = action.payload;
+            let foundProducts = action.payload.data;
             return foundProducts;
-        // case SORT_PREVIOUSLY_FETCHED_PRODUCTS:
-        //     if(action.payload === 'asc') {
-        //         return state.slice().sort((a, b) => a.price > b.price);
-        //     } else {
-        //         return state.slice().sort((a, b) => a.price < b.price);
-        //     }
-        // case FILTER_PREVIOUSLY_FETCHED_PRODUCTS:
-        //     return state.filter(product => product.category === action.payload)
         default:
             return state;
     }

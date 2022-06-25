b9b96ed78a3459a852d15259913838f527007f64
Alex Rodgers <skyewulff@outlook.com>
2022-06-24 21:51:06 -0400

    import Controls and ProductList components into App component

--- product-list-app/src/components/App.js
+++ product-list-app/src/components/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Controls from './Controls';
+import ProductList from './ProductList';
 
 function App() {
   return (
     <div className="App">
-      App component
+      <Controls />
+      <ProductList />
     </div>
   );
 }

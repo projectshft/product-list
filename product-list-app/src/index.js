a443a4b259f932a8e01315751dc535eff0af879d
Alex Rodgers <skyewulff@outlook.com>
2022-06-24 21:52:00 -0400

    create store in index.js

--- product-list-app/src/index.js
+++ product-list-app/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import productsReducer from './reducers';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={createStore(productsReducer)}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );

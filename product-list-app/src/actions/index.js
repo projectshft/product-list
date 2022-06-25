614a032efdcab7ddfb76ab67a9763facc8d54c33
Alex Rodgers <skyewulff@outlook.com>
2022-06-25 18:33:21 -0400

    tweak fetchProducts to return an object containing the search results and the submitted query params

--- product-list-app/src/actions/index.js
+++ product-list-app/src/actions/index.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
-export const SORT_PREVIOUSLY_FETCHED_PRODUCTS = "SORT_PREVIOUSLY_FETCHED_PRODUCTS";
-export const FILTER_PREVIOUSLY_FETCHED_PRODUCTS = "FILTER_PREVIOUSLY_FETCHED_PRODUCTS";
-
 const generateQueryParamsString = options => {
-    let queryParamsString = '';
+    let queryParamsString = '?';
     if(options.category) {
         queryParamsString = queryParamsString.concat('category=', options.category);
     }
@@ -21,22 +18,18 @@ const generateQueryParamsString = options => {
         }
         queryParamsString = queryParamsString.concat('query=', options.query);
     }
-    if(queryParamsString.length > 0) {
-        queryParamsString = '?' + queryParamsString;
-    }
+    queryParamsString += `${queryParamsString.length > 1 ? '&' : ''}page=${options.page}`;
     return queryParamsString;
 }
 
 export const fetchProducts = options => {
-    console.log('fetching');
-    console.dir(options);
-    const products = axios.get(`http://localhost:8000/products${generateQueryParamsString(options)}`).catch(error => {
+    const response = axios.get(`http://localhost:8000/products${generateQueryParamsString(options)}`).catch(error => {
         console.log('there was an error processing the GET request');
         return null;
     });
 
     return {
         type: FETCH_PRODUCTS,
-        payload: products
+        payload: response
     };
 }

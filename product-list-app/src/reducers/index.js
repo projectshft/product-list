47f8ace37e159cfffa8db8b45a7e08ecd935147b
Alex Rodgers <skyewulff@outlook.com>
2022-06-25 18:34:31 -0400

    change state stored with productsReducer to contain both products and query parameters

--- product-list-app/src/reducers/index.js
+++ product-list-app/src/reducers/index.js
@@ -1,12 +1,18 @@
-// import { FETCH_PRODUCTS, FILTER_PREVIOUSLY_FETCHED_PRODUCTS, SORT_PREVIOUSLY_FETCHED_PRODUCTS} from "../actions";
 import { FETCH_PRODUCTS} from "../actions";
-const DEFAULT_STATE = [];
+const DEFAULT_STATE = {
+    options: {},
+    products: []
+};
 
 const productsReducer = (state = DEFAULT_STATE, action) => {
     switch (action.type) {
         case FETCH_PRODUCTS:
-            let foundProducts = action.payload.data;
-            return foundProducts;
+            let foundProducts = action.payload.data.products;
+            let updatedOptions = action.payload.data.options;
+            return {
+                options: updatedOptions,
+                products: foundProducts
+            };
         default:
             return state;
     }

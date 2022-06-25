52d3311a7137ff3137e588cbb13a687d5f37050c
Alex Rodgers <skyewulff@outlook.com>
2022-06-24 23:19:31 -0400

    create function in ProductList component that will take the result of the GET request and map it to a list of Product components

--- product-list-app/src/components/ProductList.js
+++ product-list-app/src/components/ProductList.js
@@ -2,11 +2,18 @@ import { useSelector } from 'react-redux';
 import Product from './Product';
 import './ProductList.css';
 
+const displayProducts = productArr => {
+    if(productArr.length > 0) {
+        return productArr.map(product => <span>{product.name}</span>)
+    }
+}
+
 const ProductList = () => {
-    // const products = useSelector(state => state.products)
+    const products = useSelector(products => products);
     return (
         <div className="ProductList">
             <h1>Product List</h1>
+            {displayProducts(products)}
             <Product />
         </div>
     )

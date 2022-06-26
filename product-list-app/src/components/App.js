5c00413ff942e50902e7f2bded41d73dda092ef8
Alex Rodgers <skyewulff@outlook.com>
2022-06-25 20:21:05 -0400

    add Pagination component to App components

--- product-list-app/src/components/App.js
+++ product-list-app/src/components/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Controls from './Controls';
+import Pagination from './Pagination';
 import ProductList from './ProductList';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Controls />
       <ProductList />
+      <Pagination />
     </div>
   );
 }

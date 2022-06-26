001a6d94211e71133879ef0992faa87abf2dc20b
Alex Rodgers <skyewulff@outlook.com>
2022-06-25 20:35:20 -0400

    fix bug in last commit that fails to give page numbers to initial pagination buttons

--- product-list-app/src/index.js
+++ product-list-app/src/index.js
@@ -10,7 +10,7 @@ import { fetchProducts } from './actions';
 
 const createStoreWithPromiseMiddleware = applyMiddleware(promise)(createStore);
 const store = createStoreWithPromiseMiddleware(productsReducer);
-store.dispatch(fetchProducts({}));
+store.dispatch(fetchProducts({page: '1'}));
 
 ReactDOM.render(
     <React.StrictMode>

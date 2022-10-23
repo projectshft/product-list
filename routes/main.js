dd2530459aafd6ad0de8716bda5d04efd27a568c
RyanCaimano <ryancaimano@gmail.com>
2022-10-23 18:34:47 -0500

    Testing GIT

--- routes/main.js
+++ routes/main.js
@@ -21,73 +21,74 @@ router.get('/generate-fake-data', (req, res, next) => {
 	res.end();
 });
 
-router.get('/products', (req, res, next) => {
-	const perPage = 9;
-	// return the first page by default
-	const page = req.query.page || 1;
-	// const lowerCategory = req.query.category;
-	// const category = lowerCategory.charAt(0).toUpperCase() + lowerCategory.slice(1);
-	const category = req.query.category;
-	const price = req.query.price;
-	const search = req.query.query;
 
-	if (search) {
-		Product
-			.find({ $text: { $search: search } })
-			.skip((perPage * page) - perPage)
-			.limit(perPage)
-			.exec((err, products) => {
-				Product.count({ $text: { $search: search } }).exec((err, count) => {
-					if (err) return next(err);
-					res.send(products);
-				});
-			});
-	} else if (category) {
-		Product.find({ category: category })
-			.skip(perPage * page - perPage)
-			.limit(perPage)
-			.exec((err, products) => {
-				Product.count({ category: category }).exec((err, count) => {
-					if (err) return next(err);
-					res.send(products);
-				});
-			});
-	} else if (price) {
-		if (price === 'highest') {
-			Product.find({})
-				.sort({ price: -1 })
-				.skip(perPage * page - perPage)
-				.limit(perPage)
-				.exec((err, products) => {
-					Product.count().exec((err, count) => {
-						if (err) return next(err);
-						res.send(products);
-					});
-				});
-		} else if (price === 'lowest') {
-			Product.find({})
-				.sort({ price: 1 })
-				.skip(perPage * page - perPage)
-				.limit(perPage)
-				.exec((err, products) => {
-					Product.count().exec((err, count) => {
-						if (err) return next(err);
-						res.send(products);
-					});
-				});
-		}
-	} else {
-		Product.find({})
-			.skip(perPage * page - perPage)
-			.limit(perPage)
-			.exec((err, products) => {
-				Product.count().exec((err, count) => {
-					if (err) return next(err);
-					res.send(products);
-				});
-			});
-	}
-});
+// router.get('/products', (req, res, next) => {
+// 	const perPage = 9;
+// 	// return the first page by default
+// 	const page = req.query.page || 1;
+// 	// const lowerCategory = req.query.category;
+// 	// const category = lowerCategory.charAt(0).toUpperCase() + lowerCategory.slice(1);
+// 	const category = req.query.category;
+// 	const price = req.query.price;
+// 	const search = req.query.query;
+
+// 	if (search) {
+// 		Product
+// 			.find({ $text: { $search: search } })
+// 			.skip((perPage * page) - perPage)
+// 			.limit(perPage)
+// 			.exec((err, products) => {
+// 				Product.count({ $text: { $search: search } }).exec((err, count) => {
+// 					if (err) return next(err);
+// 					res.send(products);
+// 				});
+// 			});
+// 	} else if (category) {
+// 		Product.find({ category: category })
+// 			.skip(perPage * page - perPage)
+// 			.limit(perPage)
+// 			.exec((err, products) => {
+// 				Product.count({ category: category }).exec((err, count) => {
+// 					if (err) return next(err);
+// 					res.send(products);
+// 				});
+// 			});
+// 	} else if (price) {
+// 		if (price === 'highest') {
+// 			Product.find({})
+// 				.sort({ price: -1 })
+// 				.skip(perPage * page - perPage)
+// 				.limit(perPage)
+// 				.exec((err, products) => {
+// 					Product.count().exec((err, count) => {
+// 						if (err) return next(err);
+// 						res.send(products);
+// 					});
+// 				});
+// 		} else if (price === 'lowest') {
+// 			Product.find({})
+// 				.sort({ price: 1 })
+// 				.skip(perPage * page - perPage)
+// 				.limit(perPage)
+// 				.exec((err, products) => {
+// 					Product.count().exec((err, count) => {
+// 						if (err) return next(err);
+// 						res.send(products);
+// 					});
+// 				});
+// 		}
+// 	} else {
+// 		Product.find({})
+// 			.skip(perPage * page - perPage)
+// 			.limit(perPage)
+// 			.exec((err, products) => {
+// 				Product.count().exec((err, count) => {
+// 					if (err) return next(err);
+// 					res.send(products);
+// 				});
+// 			});
+// 	}
+// });
 
 router.get('/products/:product', (req, res, next) => {
 	Product.find({ _id: req.params.product }, (err, product) => {

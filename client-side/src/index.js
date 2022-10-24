fb73376cadc13eca7910b5e92d29047ebd7e2d66
RyanCaimano <ryancaimano@gmail.com>
2022-10-24 14:23:58 -0500

    NPM installed previous versions of react and redux for the purposes of aligning with the curriculum.

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable no-undef */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux"; 
+import { composeWithDevTools } from "redux-devtools-extension";
+import promise from "redux-promise";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(promise)));
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

53bdf4754d335751d244ff71fcc42b54c2536245
RyanCaimano <ryancaimano@gmail.com>
2022-10-25 15:32:18 -0500

    Added dropdown menu.

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,45 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+import { BasicExample } from "./components/pageLayout.js";
+
+
 
 const App = () => {
   return (
     <div>
-      <div className="wrap">
-        <div className="search">
-          <input
-            type="text"
-            className="searchTerm"
-            placeholder="What are you looking for?"
-          ></input>
-          <button type="submit" className="searchButton">
-            <i className="fa fa-search">Find</i>
-          </button>
-        </div>
-      </div>
-      <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Dropdown button
-        </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a className="dropdown-item" href="#">
-            Action
-          </a>
-          <a className="dropdown-item" href="#">
-            Another action
-          </a>
-          <a className="dropdown-item" href="#">
-            Something else here
-          </a>
-        </div>
-      </div>
+      <BasicExample />
     </div>
   );
 };
+
 
 export default App;
--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,13 +1,16 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore, applyMiddleware } from "redux"; 
 import { composeWithDevTools } from "redux-devtools-extension";
 import promise from "redux-promise";
+
 
 //ONCE I IMPORT THE REDUCER, I CAN USE IT IN THE STORE
 // const store = createStore(reducers, composeWithDevTools(applyMiddleware(promise)));

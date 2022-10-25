7f276678f3983fa912be352a4180ee64cc953c6d
RyanCaimano <ryancaimano@gmail.com>
2022-10-25 17:09:59 -0500

    Added dropdown menus.

--- client-side/src/components/pageLayout.js
+++ client-side/src/components/pageLayout.js
@@ -1,21 +1,36 @@
 import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import React from "react";
 
-
-function BasicExample(){
+function BasicExample() {
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <div>
+      <form className="searchLine">
+        <div className="input-group" id="search">
+          <input
+            type="text"
+            className="form-control rounded"
+            placeholder="Search"
+          ></input>
+          <button type="submit" className="btn btn-outline-primary">
+            Search
+          </button>
+        </div>
+      </form>
+      <Dropdown className="drop">
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
+      </Dropdown>
+    </div>
   );
-};
+}
 
 export { BasicExample };

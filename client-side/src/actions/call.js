e5cc0d0d436e487a2ed43f8742d520ce36598c64
RyanCaimano <ryancaimano@gmail.com>
2022-10-26 15:01:16 -0500

    Search-button fires, resulting in a response object whose array is 'docs', as seen in the console...9 items, appropriately. Next up is wiring up the search button to render 9 cards.

--- client-side/src/actions/call.js
+++ client-side/src/actions/call.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-
-
 export const call = (res) => {
-  axios.get('http://localhost:8000/api/products')
-  .then(console.log('res'));
+  axios.get('http://localhost:8000/products')
+  .then(console.log(res.docs[0]));
 }

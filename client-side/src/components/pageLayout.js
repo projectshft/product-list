de4f8db5f3a142f5e4bfdbc43edf57884853b649
RyanCaimano <ryancaimano@gmail.com>
2022-10-25 20:11:55 -0500

    Labeled dropdown menu items.

--- client-side/src/components/pageLayout.js
+++ client-side/src/components/pageLayout.js
@@ -2,7 +2,6 @@ import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import React from "react";
 
-
 function BasicExample() {
   return (
     <div>
@@ -19,16 +18,36 @@ function BasicExample() {
         </div>
       </form>
       <Dropdown className="drop">
-        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <DropdownButton id="dropdown-basic-button" title="Category">
+          <Dropdown.Item href="#/action-1">Movies</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Jewelery</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Industrial</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">Clothing</Dropdown.Item>
+          <Dropdown.Item href="#/action-5">Computers</Dropdown.Item>
+          <Dropdown.Item href="#/action-6">Baby</Dropdown.Item>
+          <Dropdown.Item href="#/action-7">Books</Dropdown.Item>
+          <Dropdown.Item href="#/action-8">Kids</Dropdown.Item>
+          <Dropdown.Item href="#/action-9">Tools</Dropdown.Item>
+          <Dropdown.Item href="#/action-10">Electronics</Dropdown.Item>
+          <Dropdown.Item href="#/action-11">Games</Dropdown.Item>
+          <Dropdown.Item href="#/action-12">Toys</Dropdown.Item>
+          <Dropdown.Item href="#/action-13">Garden</Dropdown.Item>
+          <Dropdown.Item href="#/action-14">Industrial</Dropdown.Item>
+          <Dropdown.Item href="#/action-15">Grocery</Dropdown.Item>
+          <Dropdown.Item href="#/action-16">Music</Dropdown.Item>
+          <Dropdown.Item href="#/action-17">Health</Dropdown.Item>
+          <Dropdown.Item href="#/action-18">Outdoors</Dropdown.Item>
+          <Dropdown.Item href="#/action-19">Shoes</Dropdown.Item>
+          <Dropdown.Item href="#/action-20">Computers</Dropdown.Item>
+          <Dropdown.Item href="#/action-21">Sports</Dropdown.Item>
+          <Dropdown.Item href="#/action-22">Beauty</Dropdown.Item>
+          <Dropdown.Item href="#/action-23">Home</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <DropdownButton id="dropdown-basic-button" title="Price">
+          <Dropdown.Item href="#/action-1">Lowest to Highest</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Highest to Lowest</Dropdown.Item>
         </DropdownButton>
+        <br></br><br></br>
       </Dropdown>
       <br></br>
       <br></br>

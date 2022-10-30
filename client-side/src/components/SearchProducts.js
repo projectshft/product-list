3bebf9c2965e91530a8381e7768f8bed89603767
RyanCaimano <ryancaimano@gmail.com>
2022-10-30 12:25:32 -0500

    Rewrote page rendering for search bar with dropdown menus. Removed search button because previously it was causing me all sorts of problems, plus it is not shown in Aaron's eval prompt. Have yet to style the search bar and dropdown. Created an array of all optional categories in separate component file so that SearchProducts isn't as cluttered. Learned that I can just map the items to an index, which is much more convenient. UseEffect is not hooked up yet because I am not dealing with pagination quite yet. Next up is connecting SearchNew to my action folder/file.

--- client-side/src/components/SearchProducts.js
+++ client-side/src/components/SearchProducts.js
@@ -4,20 +4,26 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../actions/fetchProducts";
 import { useEffect } from "react";
-
-
+import { CategoryItems } from "./CategoryItems";
 
 //to-do: account for pagination
-const SearchProducts = ({ query, category, price, setQuery, setCategory, setPrice }) => {
+const SearchProducts = ({
+  query,
+  category,
+  price,
+  setQuery,
+  setCategory,
+  setPrice,
+}) => {
   const dispatch = useDispatch();
 
   // useEffect(() => {
   //   dispatch(fetchProducts());
   // }, []);
 
-  const handleQuery= (event) => {
+  const handleQuery = (event) => {
     setQuery(event.target.value);
-     dispatch(fetchProducts(price, category, event.target.value));
+    dispatch(fetchProducts(price, category, event.target.value));
   };
 
   const handleCategory = (event) => {
@@ -25,59 +31,32 @@ const SearchProducts = ({ query, category, price, setQuery, setCategory, setPric
     dispatch(fetchProducts(price, event.target.value, query));
   };
 
-
   const handlePrice = (event) => {
     setPrice(event.target.value);
     dispatch(fetchProducts(event.target.value, category, query));
   };
 
   return (
-    <div>
-      <form className="searchLine" onSubmit={handleSubmit}>
-        <div className="input-group" id="search">
-          <input
-            type="search"
-            name="query"
-            className="form-control rounded"
-            placeholder="Search"
-            onChange={(event) => handleSearch(event)}
-          ></input>
-          <button type="submit" className="btn btn-outline-primary">
-            Search
-          </button>
-        </div>
-      </form>
-      <div className="input-group-append">
-        <select
-          className="dropdown-toggle"
-          data-toggle="dropdown"
-          onChange= {(event) => handleCategory(event)}
-          value={search.category}
-        >
-          
-          <option className="dropdown-item" value="Electronics">
-            Electronics
+    <div className="input-group mb-8">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search for a product"
+        onChange={handleQuery}
+      />
+      <select name="category" id="select-category" onChange={handleCategory}>
+        <option value="">Filter by Category</option>
+        {CategoryItems.map((choice, index) => (
+          <option key={index} value={choice}>
+            {choice}
           </option>
-        </select>
-      </div>
-      {/* <Dropdown className="drop">
-        <DropdownButton id="dropdown-basic-button" title="Category">
-          <Dropdown.Item
-            onSelect={handleCategory}
-            value="Electronics"
-          >
-            Electronics
-          </Dropdown.Item>
-        </DropdownButton>
-        <DropdownButton id="dropdown-basic-button" title="Price">
-          <Dropdown.Item href="#/action-24">Lowest to Highest</Dropdown.Item>
-          <Dropdown.Item href="#/action-25">Highest to Lowest</Dropdown.Item>
-        </DropdownButton>
-        <br></br>
-        <br></br>
-      </Dropdown> */}
-      <br></br>
-      <br></br>
+        ))}
+      </select>
+      <select name="price" id="select-price" onChange={handlePrice}>
+        <option value="">Filter by Price</option>
+        <option value="lowest">Lowest to Highest</option>
+        <option value="highest">Highest to Lowest</option>
+      </select>
     </div>
   );
 };

bf3777757355dc6796ce9bd6a971718de8b4763f
RyanCaimano <ryancaimano@gmail.com>
2022-11-02 09:39:34 -0500

    Finally understand the code from the pagination tutorial. New files include Pagination.js, usePagination.js, and pagination.scss. This required an install of 'classnames' and 'react-pagination' dependencies. Next up is wiring pagination to App.js, setting state at a high-level, etc.

--- client-side/src/actions/fetchProducts.js
+++ client-side/src/actions/fetchProducts.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-import $ from "jquery";
 
 //to-do: account for pagination
 export const FETCH_FIRST = "FETCH_FIRST";
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 
-
 export function fetchProducts(price, category, query) {
   const request = axios.get(
-    `http://localhost:8000/products?price=${price}&category=${category}&query=${query}`
+    `http://localhost:8000/products?&query=${query}&category=${category}&price=${price}`
   );
   return {
     type: FETCH_PRODUCTS,
@@ -17,7 +15,7 @@ export function fetchProducts(price, category, query) {
 }
 
 export function fetchFirst() {
-  const request = axios.get('http://localhost:8000/products?page=1');
+  const request = axios.get("http://localhost:8000/products?page=1");
   return {
     type: FETCH_FIRST,
     payload: request,

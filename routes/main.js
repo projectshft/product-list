adb4b946a1931743fea8432dff5df36a85788c29
Damian Goodenough <dgoodenough@proton.me>
2022-12-12 14:26:01 -0800

    implement pagenation and item limit

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 const { faker } = require('@faker-js/faker');
 const Product = require('../models/product');
 
+router.get('/products', (req, res, next) => {
+const page = req.query.page || 1;
+const limit = req.query.limit || 9;
+  Product.find()
+    .skip((Number(page) - 1) * 9)
+    .limit(Number(limit))
+    .exec((error, products) => {
+      if (error) throw error;
+      res.json(products)
+      res.end()
+    })
+})
+
 router.get('/generate-fake-data', (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     const product = new Product();

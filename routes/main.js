1f76f38b666c0c0c33dae858d1f3558dadd554b2
Damian Goodenough <dgoodenough@proton.me>
2022-12-13 07:36:11 -0800

    generate fake reviews for each product

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const { faker } = require('@faker-js/faker');
 const Product = require('../models/product');
+const Review = require('../models/review')
 
 router.get('/products', (req, res, next) => {
-const page = req.query.page || 1;
-const limit = req.query.limit || 9;
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 9;
   Product.find()
     .skip((Number(page) - 1) * 9)
     .limit(Number(limit))
     .exec((error, products) => {
       if (error) throw error;
-      res.json(products)
+      res.send(products)
       res.end()
     })
 })
@@ -23,6 +24,21 @@ router.get('/generate-fake-data', (req, res, next) => {
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = 'https://via.placeholder.com/250?text=Product+Image';
+
+    for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
+      const review = new Review()
+
+      review.username = faker.name.firstName();
+      review.text = faker.lorem.sentence();
+      review.product = product._id;
+
+      review.save(err => {
+        if (err) {
+          throw err
+        }
+      })
+      product.reviews.push(review);
+    }
 
     product.save((err) => {
       if (err) throw err;

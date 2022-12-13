f4d499357d5c383238a2c9945884faa7f2fb47db
Damian Goodenough <dgoodenough@proton.me>
2022-12-13 13:32:06 -0800

    delete product and post reviews to product

--- routes/products.js
+++ routes/products.js
@@ -17,7 +17,7 @@ router.param('productId', async (req, res, next, id) => {
   } else {
     console.error(`Invalid id: ${id}`)
   }
-  
+
   next();
 })
 
@@ -25,12 +25,12 @@ router.get('/', (req, res, next) => {
   const page = req.query.page || 1;
   const limit = req.query.limit || 9;
   Product.find()
-  .skip((Number(page) - 1) * 9)
-  .limit(Number(limit))
-  .exec((error, products) => {
-    if (error) throw error;
-    res.send(products)
-  })
+    .skip((Number(page) - 1) * 9)
+    .limit(Number(limit))
+    .exec((error, products) => {
+      if (error) throw error;
+      res.send(products)
+    })
 })
 
 router.post('/', (req, res) => {
@@ -42,7 +42,7 @@ router.post('/', (req, res) => {
   product.image = 'https://via.placeholder.com/250?text=Product+Image';
 
   product.save((err, product) => {
-    if(err) {
+    if (err) {
       const response = {
         "error": true,
         "message": err
@@ -59,6 +59,19 @@ router.get('/:productId', (req, res, next) => {
   res.send(req.product);
 })
 
+router.delete('/:productId', (req, res) => {
+  Product.deleteOne({_id: req.params.productId}, (err, product) => {
+    if (err) {
+      const response = {
+        "error": true,
+        "message": err
+      }
+      res.json(response);
+    }
+    res.send(product);
+  })
+})
+
 router.get('/:productId/reviews', async (req, res) => {
   try {
     const page = req.query.page || 1;
@@ -66,12 +79,12 @@ router.get('/:productId/reviews', async (req, res) => {
 
     await req.product
       .populate(
-        { 
-          path: 'reviews', 
-          options: { 
-            skip: (Number(page) - 1) * limit, 
-            limit 
-          } 
+        {
+          path: 'reviews',
+          options: {
+            skip: (Number(page) - 1) * limit,
+            limit
+          }
         })
 
     res.send(req.product.reviews)
@@ -80,7 +93,31 @@ router.get('/:productId/reviews', async (req, res) => {
   }
 })
 
+router.post('/:productId/reviews', async (req, res) => {
+  const review = new Review();
+
+  review.username = req.body.username;
+  review.text = req.body.text;
+  review.product = req.product._id;
+
+  req.product.reviews.push(review);
 
 
+  req.product.save((err, product) => {
+    if (err) {
+      throw err;
+    };
+  })
+  review.save((err, review) => {
+    if (err) {
+      const response = {
+        "error": true,
+        "message": err
+      }
+      res.json(response);
+    }
+    res.send(review);
+  })
+})
 
 module.exports = router;

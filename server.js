a0f82c5be39d434323708bd59fc7d5ecb296f4fd
Damian Goodenough <dgoodenough@proton.me>
2022-12-13 13:02:11 -0800

    add CORS

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ mongoose.connect('mongodb://localhost/products', {
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({

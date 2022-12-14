71a30fb3eaf5d203ef627964310e8d91f2914aed
Damian Goodenough <dgoodenough@proton.me>
2022-12-14 14:11:21 -0800

    change state of the products when page num is clicked

---
+++ client/src/components/PageNumbers.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+
+import { fetchProducts } from '../helpers/fetchData';
+import { setProducts } from '../actions';
+
+const PageNumbers = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { numProducts } = useSelector(state => state.products);
+  const perPage = 9;
+  const numPages = numProducts / perPage;
+
+  const {category, priceSort} = useSelector(state => state.filters);
+
+  const dispatch = useDispatch();
+
+  const handlePageChange = async (e) => {
+    setCurrentPage(Number(e.target.id) + 1);
+
+    const page = Number(e.target.id) + 1;
+
+    const products = await fetchProducts(category, priceSort,page);
+
+    dispatch(setProducts(products))
+  }
+
+  const renderPageNumbers = () => {
+    let pageNumbers = []
+    for (let i = 0; i < numPages; i++) {
+      pageNumbers.push(
+        <Col
+          onClick={e => handlePageChange(e)}
+          key={i} id={i}
+          as='a' className="page-number"
+        >
+          {i + 1}
+        </Col>
+      )
+    }
+    return pageNumbers;
+  }
+  return (
+    <Row className="mb-5 mt-2 justify-content-md-center">
+      {renderPageNumbers()}
+      <div className='text-center'>
+        Current Page: {currentPage}
+      </div>
+    </Row>
+  );
+}
+
+export default PageNumbers;

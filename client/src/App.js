dc810776ef701316c12efed7e0d798c6c8862b2a
Damian Goodenough <dgoodenough@proton.me>
2022-12-14 11:44:45 -0800

    set state of products on initial load

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import { Container } from 'react-bootstrap'
-// import {  } from 'react-redux';
 import { useEffect } from 'react';
+import {useDispatch} from 'react-redux';
 
 import Search from './components/Search';
 import ProductList from './components/ProductList';
 import PageNumbers from './components/PageNumbers';
 
 import {fetchProducts} from './helpers/fetchData'
+import {setProducts} from './actions'
 
 function App() {
+  const dispatch = useDispatch();
+
   const initialLoad = async() => {
-    fetchProducts();
+    const products = await fetchProducts();
+
+    dispatch(setProducts(products))
   }
   
   useEffect(() => {
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 export const CHANGE_CATEGORY = 'CHANGE_CATEGORY';
 export const CHANGE_PRICE_SORT = 'CHANGE_PRICE_SORT'
+export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export function changeCategoryFilter(category) {
   return {
@@ -12,5 +13,12 @@ export function changePriceSortFilter(sortMethod) {
   return {
     type: CHANGE_PRICE_SORT,
     payload: sortMethod,
+  }
+}
+
+export function setProducts(products) {
+  return {
+    type: SET_PRODUCTS,
+    payload: products
   }
 }

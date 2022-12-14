536b9b6bb7ce01ea8ede49480d714ef90b520721
Damian Goodenough <dgoodenough@proton.me>
2022-12-14 09:41:05 -0800

    set up redux store with promise

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
+import { applyMiddleWare, createStore } from 'redux';
+
+import reducers from './reducers'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const createStoreWithMiddleWare = applyMiddleWare(promise)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleWare(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

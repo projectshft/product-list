3f04c4d455f50116cad439fad573ce00c57d6009
Damian Goodenough <dgoodenough@proton.me>
2022-12-14 11:02:56 -0800

    dispatch filter options on change

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,21 +1,27 @@
 import { Form, Button, Row, Col, InputGroup } from 'react-bootstrap'
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { changeCategoryFilter } from '../actions';
+import { changePriceSortFilter } from '../actions';
 
 const Search = () => {
   const [query, setQuery] = useState('')
   const [category, setCategory] = useState(null)
   const [priceSort, setPriceSort] = useState(null)
 
+  const dispatch = useDispatch();
+
   const handleSearchChange = (e) => setQuery(e.target.value);
 
   const handleCategoryChange = (e) => {
-    setCategory(e.target.value);
-    console.log(category)
+    dispatch(changeCategoryFilter(e.target.value))
   }
   
   const handlePriceSortChange = (e) => {
-    setPriceSort(e.target.value);
-    console.log(priceSort)
+    dispatch(changePriceSortFilter(e.target.value))
+    // setPriceSort(e.target.value);
+    // console.log(priceSort)
   }
 
   return (
@@ -35,7 +41,7 @@ const Search = () => {
         </Form.Group>
         <Form.Group as={Col} md={3}>
           <Form.Select value={priceSort} onChange={(e) => handlePriceSortChange(e)} as={Col}>
-            <option value={null}>Sort By Price</option>
+            <option value=''>Sort By Price</option>
             <option value='highest'>Highest to lowest</option>
             <option value='lowest'>Lowest to Highest</option>
           </Form.Select>

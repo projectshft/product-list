cd7265e2efd7f6599ef41370696c6eec67372b16
Damian Goodenough <dgoodenough@proton.me>
2022-12-14 12:08:19 -0800

    use redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers'
 
@@ -11,11 +12,12 @@ import './index.css';
 import App from './App';
 
 const createStoreWithMiddleWare = applyMiddleware(promise)(createStore);
+const store = createStoreWithMiddleWare(reducers, composeWithDevTools())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={createStoreWithMiddleWare(reducers)}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>

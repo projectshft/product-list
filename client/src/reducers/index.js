58e3ac7c607d2b49b6820df8d3f1f8c7a96d43c5
Damian Goodenough <dgoodenough@proton.me>
2022-12-15 07:34:12 -0800

    seperate imports

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
+
 import filterReducer from './filterReducer';
 import productReducer from './productReducer';
 import categoryReducer from './categoryReducer';

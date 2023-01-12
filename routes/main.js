b77e475c1f655b85d29095ad2c7666451a45774d
Jill Arnold <jillarnold2913@gmail.com>
2023-01-12 16:49:15 -0500

    Created new product and new product's review

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const {faker} = require("@faker-js/faker");
 const MyProducts = require("../models/myProducts");
 const Reviews = require('../models/reviews')
 
+//LOAD DB WITH FAKE DATA 
 // router.get("/generate-fake-data", (req, res, next) => {
 //   for (let i = 0; i < 90; i++) {
 //     let myProducts = new MyProducts();
@@ -19,25 +20,44 @@ const Reviews = require('../models/reviews')
 //   res.end();
 // });
 
-router.get("/myProducts", (req, res, next) => {
-  const perPage = 9;
+//IMPLEMENT PAGINATION 
+// router.get("/myProducts", (req, res, next) => {
+//   const perPage = 9;
 
-  // return the first page by default
-  const page = req.query.page || 1;
+//   // return the first page by default
+//   const page = req.query.page || 1;
 
-  MyProducts.find({})
-    .skip(perPage * page - perPage)
-    .limit(perPage)
-    .exec((err, myProducts) => {
-      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
-      MyProducts.count().exec((err, count) => {
-        if (err) return next(err);
+//   MyProducts.find({})
+//     .skip(perPage * page - perPage)
+//     .limit(perPage)
+//     .exec((err, myProducts) => {
+//       // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+//       MyProducts.count().exec((err, count) => {
+//         if (err) return next(err);
 
-        res.send(myProducts);
-        });
-        console.log(myProducts)
-    });
+//         res.send(myProducts);
+//         });
+//         console.log(myProducts)
+//     });
+// });
+
+//CREATE NEW PRODUCT
+let newProduct = new MyProducts({
+  category: 'Health',
+  name: 'Cool Purple Medicine Ball',
+  price: 25,
+  image: 'https://via.placeholder.com/250?text=Product+Image',
+  reviews: []
 });
+
+let newProductReview = new Reviews({
+  userName: 'jillannette',
+  text: 'Great medicine ball for the price!',
+  product: newProduct._id,
+});
+
+newProduct.save();
+newProductReview.save();
 
 
 module.exports = router;

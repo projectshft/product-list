c7c7b806b8d6a78b230bd554220c1daa2388b4c4
Jill Arnold <jillarnold2913@gmail.com>
2023-01-12 11:59:38 -0500

    Loaded DB and added pagination code

--- routes/main.js
+++ routes/main.js
@@ -1,21 +1,42 @@
 const router = require("express").Router();
 const {faker} = require("@faker-js/faker");
-const Product = require("../models/product");
+const Products = require("../models/products");
 
-router.get("/generate-fake-data", (req, res, next) => {
-  for (let i = 0; i < 90; i++) {
-    let product = new Product();
+// router.get("/generate-fake-data", (req, res, next) => {
+//   for (let i = 0; i < 90; i++) {
+//     let products = new Products();
 
-    product.category = faker.commerce.department();
-    product.name = faker.commerce.productName();
-    product.price = faker.commerce.price();
-    product.image = "https://via.placeholder.com/250?text=Product+Image";
+//     products.category = faker.commerce.department();
+//     products.name = faker.commerce.productName();
+//     products.price = faker.commerce.price();
+//     products.image = "https://via.placeholder.com/250?text=Product+Image";
 
-    product.save((err) => {
-      if (err) throw err;
+//     products.save((err) => {
+//       if (err) throw err;
+//     });
+//   }
+//   res.end();
+// });
+
+router.get("/myProducts", (req, res, next) => {
+  const perPage = 9;
+
+  // return the first page by default
+  const page = req.query.page || 1;
+
+  Products.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+      Products.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+        });
+        console.log(products)
     });
-  }
-  res.end();
 });
+
 
 module.exports = router;

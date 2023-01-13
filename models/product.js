54dd13b5e17ec981b67c898eec49769a99c09c45
Daniel Bui <dnlbui@proton.me>
2023-01-13 13:55:19 -0600

    changed schema to have compound index

--- models/product.js
+++ models/product.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  category: String,
-  name: String,
-  price: Number,
-  image: String,
-  reviews: [{type: Schema.Types.ObjectId, ref: "Review"}]
+  "category": String,
+  "name": String,
+  "price": Number,
+  "image": String,
+  "reviews": [{type: Schema.Types.ObjectId, ref: "Review"}]
 });
 
 const ReviewSchema = new Schema({
@@ -14,6 +14,8 @@ const ReviewSchema = new Schema({
   text: String,
   product: [{type: Schema.Types.ObjectId, ref: "Product"}],
 })
+
+ProductSchema.index({"name": 'text', "category": 'text'});
 
 
 module.exports = {Product: mongoose.model("Product", ProductSchema), Review: mongoose.model("Review", ReviewSchema)};

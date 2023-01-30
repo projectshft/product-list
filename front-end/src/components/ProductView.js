cacf2d90df344174ef59d5bbd87717f3d131db13
funkee <xxtrm98@protonmail.com>
2023-01-30 11:38:00 -0800

    created dynamic pages

--- front-end/src/components/ProductView.js
+++ front-end/src/components/ProductView.js
@@ -5,35 +5,43 @@ import "./ProductViewStyles.css";
 function ProductView(props) {
   const products = useSelector((state) => state.product.products.product);
   const count = useSelector((state) => state.product.products.count);
-
-  // Use this to dynamically create an array of numbers that will populate according to product and map in the DOM.
-  // const numOfPages = Math.ceil(count / 9);
+  const numOfPages = Math.ceil(count / 9);
 
   //dynamically create links that will set the value of the page into the prop.setPageChange(x)
-  // const handleThePage = () => {
-  //   props.setPageChange();
-  // };
+
+  const pageNumbers = [];
+  for (let i = 1; i <= numOfPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const listedPages = pageNumbers?.map((product, index) => {
+    return (
+      <div key={index} className="page-format">
+        <div>
+          <h3 value={product}>{product}</h3>
+        </div>
+      </div>
+    );
+  });
+
   const listedProduct = products?.map((product, index) => {
     return (
       <div key={index} className="grid-item">
         <div className="test">
           Category: {product.category} <h2 id="other-test">{product.price}</h2>
         </div>
-
-        <img alt="product" src={product.image}></img>
+        {/* <img alt="product" src={product.image}></img> */}
         <h3>{product.name}</h3>
       </div>
     );
   });
+
   return (
     <div>
       <div className="grid">{listedProduct}</div>
       {/* below is a test to see that handleThePage works. */}
-      <div className="pages">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-      </div>
+      {/* <div className="pages">{numOfPages ? numOfPages : null}</div> */}
+      <div className="page-format">{listedPages}</div>
     </div>
   );
 }

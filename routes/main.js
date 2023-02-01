6731948539552b8be3bf2d1abb62f1d1eabfb3d6
Ezra Godde <ezragodde@Ezras-MacBook-Air.local>
2023-02-01 09:40:47 -0800

    some routes working, posting review not working

---
+++ routes/main.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const router = require("express").Router();
+const faker = require("faker");
+const mongoose = require("mongoose");
+const Product = require("../models/product");
+const Review = require("../models/review");
+const bodyParser = require('body-parser');
+let query = Product.find();
+
+
+router.get("/generate-fake-data", (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product();
+
+    product.category = faker.commerce.department();
+    product.name = faker.commerce.productName();
+    product.price = faker.commerce.price();
+    product.image = "https://via.placeholder.com/250?text=Product+Image";
+    product.reviews = 
+
+    product.save((err) => {
+      if (err) throw err;
+    });
+  }
+  res.end();
+});
+
+//step 1 delete all fake data from products
+//step 2 check if ref is done correctly 
+//step 3 build out all the routes, colon means they want id
+
+
+
+router.get("/products", (req, res, next) => {
+  const perPage = 9;
+
+  // return the first page by default
+  const page = req.query.page || 1;
+
+  Product.find({})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      // Note that we're not sending `count` back at the moment, but in the future we might want to know how many are coming back so we can figure out the number of pages
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+
+        res.send(products);
+      });
+    });
+    // console.log(mongoose.Types.ObjectId.isValid('63d314c66dc35b7f063121c8'));
+});
+
+
+
+
+//product id working
+router.get('/product/:productId', (req, res) => {
+  const id = req.params.productId;
+  console.log(id, 'id')
+
+  Product.findById(id)
+    .exec((err, product) => {
+      if (err) throw err;
+      res.send(product);
+      console.log(product, 'product');
+      console.log(req.params.productId, 'product id');
+  });
+});
+
+
+
+//GET /products/:product/reviews: Returns ALL the reviews for a product, but limited to 4 at a time. This one will be a little tricky as you'll have to retrieve them out of the products. You should be able to pass in an optional page query parameter to paginate.
+router.get('/product/:productId/reviews', (req, res) => {
+  const perPage = 4;
+  const page = req.query.page || 1;
+  const id = req.params.productId;
+  console.log(id, 'id')
+
+  Product.findById(id)
+    .skip(perPage * page - perPage)
+    .limit(perPage)  
+    .exec((err, product) => {
+      if (err) throw err;
+      res.send(product.reviews);
+      console.log(product, 'product');
+      console.log(product.reviews, 'product reviews');
+  });
+})
+//intro and body parser parsity vid with post/ postman
+//POST /products/:product/reviews: Creates a new review in the database by adding it to the correct product's reviews array.
+
+router.post("/products/:productId/reviews", (req, res) => {
+  const id = req.params.productId;
+  console.log(id, 'id');
+  // console.log(mongoose.Types.ObjectId.isValid('63d314c66dc35b7f063121c8'));
+  
+  Product.findById( id , (err, products) => {
+    products.reviews.push({
+      userName: req.body.userName,
+      text: req.body.text,
+      productId: req.body,
+    });
+    products.save((err) => {
+      if (err) throw err;
+    });
+  });
+});
+
+module.exports = router;

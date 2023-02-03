659de5ff806b4bb4eba7fe2d7a73879590813fe4
Jill Arnold <jillarnold2913@gmail.com>
2023-02-03 13:38:41 -0500

    added CORS, React, Redux

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ mongoose.connect(mongoDB, {
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -20,6 +30,8 @@ app.use(
 const mainRoutes = require("./routes/main");
 
 app.use(mainRoutes);
+
+
 
 app.listen(8000, () => {
   console.log("Node.js listening on port " + 8000);

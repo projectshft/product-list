e4f9132cf35dafb54b36445ae949822b32de980a
Ezra Godde <ezragodde@Ezras-MacBook-Air.local>
2023-02-06 20:18:47 -0800

    reviews paginate not working

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 const Review = require("../models/review");
 const bodyParser = require('body-parser');
+const product = require('../models/product');
+const { request } = require('express');
 let query = Product.find();
 
 
@@ -16,7 +18,7 @@ router.get("/generate-fake-data", (req, res, next) => {
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
-    product.reviews = 
+    product.reviews = [];
 
     product.save((err) => {
       if (err) throw err;
@@ -24,11 +26,6 @@ router.get("/generate-fake-data", (req, res, next) => {
   }
   res.end();
 });
-
-//step 1 delete all fake data from products
-//step 2 check if ref is done correctly 
-//step 3 build out all the routes, colon means they want id
-
 
 
 router.get("/products", (req, res, next) => {
@@ -70,6 +67,9 @@ router.get('/product/:productId', (req, res) => {
 
 
 
+
+
+
 //GET /products/:product/reviews: Returns ALL the reviews for a product, but limited to 4 at a time. This one will be a little tricky as you'll have to retrieve them out of the products. You should be able to pass in an optional page query parameter to paginate.
 router.get('/product/:productId/reviews', (req, res) => {
   const perPage = 4;
@@ -87,24 +87,71 @@ router.get('/product/:productId/reviews', (req, res) => {
       console.log(product.reviews, 'product reviews');
   });
 })
-//intro and body parser parsity vid with post/ postman
-//POST /products/:product/reviews: Creates a new review in the database by adding it to the correct product's reviews array.
+//not working showing an empty array
 
+
+
+//working
 router.post("/products/:productId/reviews", (req, res) => {
-  const id = req.params.productId;
-  console.log(id, 'id');
-  // console.log(mongoose.Types.ObjectId.isValid('63d314c66dc35b7f063121c8'));
+  let newReview = new Review();
+
+  newReview.userName = req.body.userName;
+  newReview.text = req.body.text;
+  newReview.productId = req.body.productId;
+
+  const productId = req.params.productId;
   
-  Product.findById( id , (err, products) => {
-    products.reviews.push({
-      userName: req.body.userName,
-      text: req.body.text,
-      productId: req.body,
-    });
-    products.save((err) => {
+console.log(productId, 'product id');
+console.log(newReview.productId, 'new review id');
+
+  if(productId === newReview.productId) {
+    newReview.save((err) => {
       if (err) throw err;
     });
-  });
+    res.send(newReview)
+  }
 });
+
+
+
+//working
+router.post("/products", (req, res) => {
+  let newProduct = new Product();
+
+  newProduct.name = req.body.name;
+  newProduct.category = req.body.category;
+  newProduct.price = req.body.price;
+  newProduct.image = req.body.image;
+
+  newProduct.save((err) => {
+      if (err) throw err;
+    });
+  res.send(newProduct);
+});
+
+
+
+//working
+router.delete("/products/:productId", (req, res) => {
+  let productId = req.params.productId;
+  Product
+    .deleteOne({ _id: productId })
+    .then(() => {
+      res.send();
+    });
+});
+
+//working delete object id of review
+router.delete("/reviews/:review", (req, res) => {
+  let reviewId = req.params.review;
+  Review
+    .deleteOne({ _id: reviewId })
+    .then(() => {
+      res.send();
+    });
+});
+
+
+
 
 module.exports = router;

5317b4931c04af46b50a122fde7272c982284deb
Nils Landsberg <nils.landsberg78@gmail.com>
2023-03-06 23:29:13 -0500

    Initial commit

---
+++ routes/main.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const faker = require("faker");
+const Product = require("../models/product");
+
+router.get("/generate-fake-data", (req, res, next) => {
+  for (let i = 0; i < 90; i++) {
+    let product = new Product();
+
+    product.category = faker.commerce.department();
+    product.name = faker.commerce.productName();
+    product.price = faker.commerce.price();
+    product.image = "https://via.placeholder.com/250?text=Product+Image";
+
+    product.save();
+  }
+  res.end();
+});
+
+router.get("/products", (req, res, next) => {
+  run()
+  async function run() {
+    try {
+      let products = await Product.find().limit(9);
+      res.send(products);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }  
+});
+
+
+module.exports = router;

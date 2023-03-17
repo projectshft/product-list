c36d4958dda7de67f17c95d0156018ee9ebac59c
josephl37 <josephliow2000@gmail.com>
2023-03-17 10:55:26 -0500

    fixed reviews bug

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,34 @@
 const router = require("express").Router();
 const faker = require("faker");
 const Product = require("../models/product");
+const Review = require("../models/review");
 
 router.get("/generate-fake-data", (req, res, next) => {
   for (let i = 0; i < 90; i++) {
     let product = new Product();
+    let review = new Review();
 
     product.category = faker.commerce.department();
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
+    
+    review.userName = faker.name.firstName();
+    review.text = faker.lorem.sentence();
+    review.product = product._id;
+
+    product.reviews.push(review);
 
     product.save()
       .catch((err) => {
         throw err;
       })
+
+    review.save()
+      .catch((err) => {
+        throw err;
+      })
+
   }
   res.end();
 });

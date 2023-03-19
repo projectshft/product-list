1b7ce4181fe84d16993fc8e144aaafa5a3054f89
josephl37 <josephliow2000@gmail.com>
2023-03-19 14:33:56 -0500

    fixed bug

--- frontend/product-fe/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import ProductReducer from "./product-reducer";
-
-const rootReducer = combineReducers({
-  product: ProductReducer,
-})
-
-export default rootReducer;

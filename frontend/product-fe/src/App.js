8e13bd48e24d3310d6b899eb5ec21598d58578e4
josephl37 <josephliow2000@gmail.com>
2023-03-19 13:48:45 -0500

    added styling for pagination

--- frontend/product-fe/src/App.js
+++ frontend/product-fe/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from "./components/navbar"
 import Products from "./components/products"
+import Footer from "./components/footer"
 
 function App() {
   return (
     <div>
       <Nav />
       <Products />
+      <Footer />
     </div>
   );
 }

74ab49afa6f4fb0b4abe3a597f0e73554b20db4c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-18 22:26:21 -0400

    able to get review by product ID

--- models/product.js
+++ models/product.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
+
+const AuthorSchema = new Schema({
+  name: String,
+  reviews: [{type: Schema.Types.ObjectId, ref: "review"}]
+})
+
+const ReviewSchema = new Schema({
+  review: String,
+  author: [{type: Schema.Types.ObjectId, ref: "author" }],
+  product: [{type: Schema.Types.ObjectId, ref: "Product" }]
+
+})
+
 const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
   image: String,
+  reviews: [{type: Schema.Types.ObjectId, ref: "review"}]
 });
 
+module.exports = mongoose.model("review", ReviewSchema);
+module.exports = mongoose.model("author", AuthorSchema);
 module.exports = mongoose.model("Product", ProductSchema);
--- routes/main.js
+++ routes/main.js
@@ -10,6 +10,7 @@ router.get("/generate-fake-data", (req, res, next) => {
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
+    product.reviews = [];
 
     const result = product.save();
     console.log(result)
@@ -22,21 +23,19 @@ router.get("/generate-fake-data", (req, res, next) => {
 });
 
 //New way to fix no longer accepts a callback()
+//Gets 9 products per page
 router.get("/products",async (req, res, next) => {
   try {
     //Default val for page and result per page
     const page = parseInt(req.query.page) || 1;
     const resultsPerPage = 9;
-
     //Skip and limit opt for pagination the results
     const options = {
       skip: (page -1) * resultsPerPage,
       limit: resultsPerPage
     };
-
     const products = await Product.find({}, null, options);
     const totalResults = await Product.countDocuments();
-
     //Response Obj with pagination details
     const response = {
       total: totalResults,
@@ -47,12 +46,54 @@ router.get("/products",async (req, res, next) => {
     };
     res.send(response);
     console.log(response);
-
   } catch (err) {
     console.log(err);
     res.status(500).send({error: "An unable to GET data"})
   }
 });
+
+//Gets Product by ID
+router.get("/products/:product", async (req, res, next) => {
+  try {
+    const prodById = await Product.findById(req.params.product);
+
+    if (!prodById) {
+      return res.status(404).send({error: "404 Not Found No Product with that ID"});
+    }
+    res.send(prodById);
+    console.log(prodById);
+
+  } catch (err) {
+    console.log (err);
+    res.status(500).send({error: "error has occured"});
+  }
+});
+
+//Get Product Reviews by Product ID
+router.get("/products/:product/reviews", async (req, res, next) => {
+  try {
+    const prodById = await Product.findById(req.params.product);
+    if (!prodById) {
+      return res.status(404).send({error: "404 Not Found No Product with that ID"});
+    }
+    //Data for Result Obj
+    const pageNumber = parseInt(req.query.page) || 1;
+    const reviewNumber = 4;
+    const start = (pageNumber - 1) * reviewNumber;
+    const end = start + reviewNumber;
+    const allReviews = prodById.reviews.slice(start, end);
+    //Result  Obj
+    const result = {
+      page: pageNumber,
+      onPage: reviewNumber,
+      reviews: allReviews
+    }
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({error: "Error Occured"})
+  }
+})
 
 
 module.exports = router;

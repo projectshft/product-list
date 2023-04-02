48b5f546476b70ba2f4a7f1cee3d1d70206f18e6
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-04-01 23:06:26 -0400

    fixed call so it is just returning arrays of the products

--- react_app/src/actions/products.js
+++ react_app/src/actions/products.js
@@ -4,10 +4,8 @@ import axios from "axios";
 export const fetchTest = () => async (dispatch) => {
   try {
     const response = await axios.get("http://localhost:8000/products")
-    const result = await response
+    const result = await response.data.products
     console.log("result", result)
-    const resultData = result.products
-    console.log(resultData);
     dispatch ({type: FETCH_PRODUCTS, payload: {products: result}})
   } catch (error) {
     console.error("data error", error)

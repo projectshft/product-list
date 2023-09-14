749aee6beaf163c0ec7d9460e38aa10b5a63f8b4
etiry <7997357+etiry@users.noreply.github.com>
2023-09-14 10:42:07 -0400

    Fix typos

--- models/product.js
+++ models/product.js
@@ -10,4 +10,4 @@ const ProductSchema = new Schema({
   reviews: [{ type: Schema.Types.ObjectId, ref: 'review' }]
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('product', ProductSchema);

a21296d132172075cd7b3b8ab0cde006db5d4e7b
etiry <7997357+etiry@users.noreply.github.com>
2023-09-14 10:14:04 -0400

    Add review schema

--- models/product.js
+++ models/product.js
@@ -6,7 +6,8 @@ const ProductSchema = new Schema({
   category: String,
   name: String,
   price: Number,
-  image: String
+  image: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'review' }]
 });
 
 module.exports = mongoose.model('Product', ProductSchema);

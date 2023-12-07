40ed6f7f3825396ceef666e4250c295e215bc4ce
Jordan Cox <jordancox747@outlook.com>
2023-12-07 16:20:50 -0700

    css

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-    <nav className="sticky bg-gray-300 p-3 border border-gray-400">
+    <nav className="sticky bg-gray-300 p-3 border border-gray-400 flex flex-wrap w-full">
       <input
         type="text"
         className="px-1 rounded border-2 border-slate-500 bg-slate-100 w-1/3"
@@ -11,11 +11,19 @@ function NavBar() {
         value=""
       />
       <button className="rounded bg-blue-900 px-2 border-2 border-blue-900 text-white hover:bg-blue-700 hover:border-blue-700 ms-2">Search</button>
-      <span className="collapse sm:visible">Category drop down     price drop down</span>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline float-right w-8 h-8 cursor-pointer sm:hidden">
+      <div className="hidden w-full sm:flex sm:items-center sm:w-auto ms-auto">
+        <ul className="flex sm:justify-between">
+          <li>
+            <a className="block mx-2" href="#">Category</a>
+          </li>
+          <li>
+            <a className="block" href="#">Price</a>
+          </li>
+        </ul>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 cursor-pointer sm:hidden ms-auto">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
       </svg>
-
     </nav>
   );
 }

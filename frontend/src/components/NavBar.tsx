59dbe60cae678e00c92c21bcdd24718c49ca205f
Jordan Cox <jordancox747@outlook.com>
2023-12-07 08:23:08 -0700

    more css

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -11,8 +11,8 @@ function NavBar() {
         value=""
       />
       <button className="rounded bg-blue-900 px-2 border-2 border-blue-900 text-white hover:bg-blue-700 hover:border-blue-700 ms-2">Search</button>
-      <span>Category drop down     price drop down</span>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline float-right w-8 h-8 cursor-pointer">
+      <span className="collapse sm:visible">Category drop down     price drop down</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline float-right w-8 h-8 cursor-pointer sm:hidden">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
       </svg>
 

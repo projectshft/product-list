f4896b2879da1d7ef4f2d09e4a57a1ee85ee2f3e
Jordan Cox <jordancox747@outlook.com>
2023-12-09 06:32:08 -0700

    basic product fetch works

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import SearchResultsItem from "./SearchResultsItem";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+import { Product } from "../../types/types.ts";
 
 function SearchResults() {
+  const [page, setPage] = useState(0);
+
+  const fetchProducts = async (page = 0) => {
+    const res = await fetch(`http://localhost:8000/products?page=${page}`); 
+    const data = await res.json();
+    return data.responseMessage;
+  };
+
+  const { isPending, isError, error, data, isFetching, isPlaceholderData } =
+    useQuery({
+      queryKey: ["products", page],
+      queryFn: () => fetchProducts(page),
+      placeholderData: keepPreviousData,
+    });
+
   return (
     <div className="columns-1 sm:columns-3 mx-auto mt-8">
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
-      <SearchResultsItem />
+      { isPending ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div>
+          {data.map((product: Product) => {
+            return <SearchResultsItem key={product._id} product={product} />
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/SearchResultsItem.tsx
+++ frontend/src/components/SearchResultsItem.tsx
@@ -1,18 +1,20 @@
-function SearchResultsItem() {
+import { Product } from "../../types/types.ts";
+
+function SearchResultsItem( { product }: { product: Product } ) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg mb-8 m-auto bg-slate-100 pb-4">
       <span className="inline-block text-2xl bg-orange-400 py-2 px-3 mt-4 mx-4 rounded-full">
-        $400
+        ${product.price}
       </span>
       <img
         className="w-3/4 m-auto pt-4"
-        src="https://via.placeholder.com/250?text=Product+Image"
+        src={product.image}
         alt="product-img"
       />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-center">Product Name</div>
+        <div className="font-bold text-xl mb-2 text-center">{product.name}</div>
         <p className="text-gray-700 text-lg">
-          <b>Category:</b> Movies
+          <b>Category:</b> {product.category}
         </p>
       </div>
     </div>

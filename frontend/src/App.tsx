8c6ce3c9d1dbb9c87f7dd3b206fe3f66b0f542d6
Jordan Cox <jordancox747@outlook.com>
2023-12-09 05:32:12 -0700

    footer design complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import NavBar from "./components/NavBar";
 import SearchResults from "./components/SearchResults";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="contaner mx-auto">
       <NavBar />
       <SearchResults />
-      <footer className="relative w-full">
-        <div className="sticky inset-x-0 bottom-0 text-center">Footer</div>
-      </footer>
+      <Footer />
     </div>
   );
 }

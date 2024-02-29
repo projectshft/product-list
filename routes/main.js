6f8ca31f992a01cd196288d0ada1627d51974f61
AdamC7313 <AdamC7313@gmail.com>
2024-02-28 20:19:29 -0500

    create tests and use new faker

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,28 @@
 const router = require("express").Router();
-const faker = require("faker");
+const { faker } = require("@faker-js/faker");
 const Product = require("../models/product");
+const Review = require("../models/review");
 
-router.get("/generate-fake-data", async (req, res, next) => {
+router.get("/api/generate-fake-data", async (req, res, next) => {
   try {
     for (let i = 0; i < 90; i++) {
+
       let product = new Product();
 
       product.category = faker.commerce.department();
       product.name = faker.commerce.productName();
       product.price = faker.commerce.price();
       product.image = "https://via.placeholder.com/250?text=Product+Image";
+      product.reviews = [];
+
+      for(let j = 0; j < 5; j++) {
+        let review = new Review();
+        review.userName = faker.internet.userName();
+        review.text = faker.lorem.sentence();
+        review.product = product._id;
+        product.reviews.push(review);
+        await review.save();
+      }
 
       await product.save();
     }
@@ -21,7 +33,7 @@ router.get("/generate-fake-data", async (req, res, next) => {
   }
 });
 
-router.get("/products", async (req, res, next) => {
+router.get("/api/products", async (req, res, next) => {
   try {
     const perPage = 9;
     const page = req.query.page || 1;
@@ -37,4 +49,11 @@ router.get("/products", async (req, res, next) => {
   }
 });
 
+router.get("/api/products/:id", async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    console.log(err)
+  }
+})
 module.exports = router;
---
+++ test/server.test.js
@@ -0,0 +1,97 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Products', () => {
+  describe('/GET products', () => {
+    it('should GET all the products', (done) => {
+      chai.request(app)
+        .get('/api/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(9);
+          done(err);
+        });
+    });
+    it('should GET specific product by id', (done) => {
+      chai.request(app)
+        .get('/api/products/65dfd1fa54d7d52a5c82e39b')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name').eql('Modern Metal Bike');
+          done(err);
+        });
+    });
+    it('should GET all the reviews for a product', (done) => { 
+      chai.request(app)
+        .get('/api/products/65dfd1fa54d7d52a5c82e39b/reviews')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(5);
+          res.body[0].should.have.property('userName').eql('Addie_Rowe');
+          done(err);
+        });
+    });
+  });
+  describe('/POST products', () => {
+    it('should POST a product', (done) => {
+      const product = {
+        category: 'Toys',
+        name: 'Toy Car',
+        price: 10,
+        image: 'https://via.placeholder.com/250?text=Product+Image',
+        reviews: [{userName: 'John Doe', text: 'Great product!'}, {userName: 'Jane Doe', text: 'Terrible product!'}]
+      };
+      chai.request(app)
+        .post('/api/products')
+        .send(product)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name').eql('Toy Car');
+          done(err);
+        });
+    });
+    it('should POST a review for a product', (done) => {
+      const review = {
+        userName: 'John Doe',
+        text: 'Great product!',
+        product: '65dfd1fa54d7d52a5c82e39b'
+      };
+      chai.request(app)
+        .post('/api/products/65dfd1fa54d7d52a5c82e39b/reviews')
+        .send(review)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('text').eql('Great product!');
+          done(err);
+        });
+    });
+  });
+  describe('/DELETE products', () => {
+    it('should DELETE a product', (done) => {
+      chai.request(app)
+        .delete('/api/products/65dfd1fa54d7d52a5c82e39b')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done(err);
+        });
+    });
+    it('should DELETE a review for a product', (done) => {
+      chai.request(app)
+        .delete('/api/reviews/65dfd1fa54d7d52a5c82e39b')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.reviews.length.should.be.eql(4);
+          done(err);
+        });
+    });
+  });
+});
